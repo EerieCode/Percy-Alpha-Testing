a64a8039077c6357e534b30d06ae20111db50f58
EerieCode <eeriecode@gmail.com>
2018-04-13 07:23:53 +0200

    CYHO-JP058 --リバース・オブ・ザ・ワールド

---
+++ CYHO/c101005058.lua
@@ -0,0 +1,65 @@
+--リバース・オブ・ザ・ワールド
+--Reverse of the World
+--Scripted by Eerie Code
+function c101005058.initial_effect(c)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,101005058+EFFECT_COUNT_CODE_OATH)
+    e1:SetTarget(c101005058.target)
+    e1:SetOperation(c101005058.activate)
+    c:RegisterEffect(e1)
+end
+function c101005058.filter(c,e,tp,m,ft)
+    if not c:IsCode(46427957,72426662) or bit.band(c:GetType(),0x81)~=0x81
+        or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
+    local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
+    if ft>0 then
+        return mg:CheckWithSumGreater(Card.GetRitualLevel,c:GetLevel(),c)
+    else
+        return mg:IsExists(c101005058.filterF,1,nil,tp,mg,c)
+    end
+end
+function c101005058.filterF(c,tp,mg,rc)
+    if c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:GetSequence()<5 then
+        Duel.SetSelectedCard(c)
+        return mg:CheckWithSumGreater(Card.GetRitualLevel,rc:GetLevel(),rc)
+    else return false end
+end
+function c101005058.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        local mg=Duel.GetRitualMaterial(tp)
+        local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+        return ft>-1 and Duel.IsExistingMatchingCard(c101005058.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp,mg,ft)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function c101005058.activate(e,tp,eg,ep,ev,re,r,rp)
+    local mg=Duel.GetRitualMaterial(tp)
+    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local tg=Duel.SelectMatchingCard(tp,c101005058.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp,mg,ft)
+    local tc=tg:GetFirst()
+    if tc then
+        mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
+        local mat=nil
+        if ft>0 then
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+            mat=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
+        else
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+            mat=mg:FilterSelect(tp,c101005058.filterF,1,1,nil,tp,mg,tc)
+            Duel.SetSelectedCard(mat)
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+            local mat2=mg:SelectWithSumGreater(tp,Card.GetRitualLevel,tc:GetLevel(),tc)
+            mat:Merge(mat2)
+        end
+        tc:SetMaterial(mat)
+        Duel.ReleaseRitualMaterial(mat)
+        Duel.BreakEffect()
+        Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
+        tc:CompleteProcedure()
+    end
+end
