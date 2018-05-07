8785aff9387551f6037db709deed2dac92ed686c
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-05-07 05:58:35 +0200

    Update c100409001.lua

--- DP20/c100409001.lua
+++ DP20/c100409001.lua
@@ -1,1 +1,85 @@
 
+--青眼の混沌龍 
+-- Blue-Eyes Chaos Dragon
+function c100409001.initial_effect(c)
+	c:EnableReviveLimit()
+	--spsummon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.ritlimit)
+	c:RegisterEffect(e1)
+	--cannot target
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+	--indes
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(c100409001.indval)
+	c:RegisterEffect(e3)
+	--pos Change
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(100409001,0))
+	e5:SetCategory(CATEGORY_POSITION)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e5:SetCondition(c100409001.poscon)
+	e5:SetTarget(c100409001.postg)
+	e5:SetOperation(c100409001.posop)
+	c:RegisterEffect(e5)
+end
+function c100409001.indval(e,re,tp)
+	return tp~=e:GetHandlerPlayer()
+end
+function c100409001.mfilter(c)
+	return c:IsCode(89631139)
+end
+function c100409001.poscon(e)
+	local c=e:GetHandler()
+	local mg=c:GetMaterial()
+	return c:GetSummonType()==SUMMON_TYPE_RITUAL and mg:GetCount()>0 and mg:IsExists(c100409001.mfilter,1,nil)
+end
+
+
+function c100409001.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE,nil)>0 end
+	local sg=Duel.GetFieldGroup(tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,sg,sg:GetCount(),0,0)
+end
+function c100409001.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local sg=Duel.GetFieldGroup(tp,0,LOCATION_MZONE,nil)
+	if sg:GetCount()>0 then
+		Duel.ChangePosition(sg,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
+		local tc=sg:GetFirst()
+		while tc do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetValue(0)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
+			tc:RegisterEffect(e1)
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_DEFENSE_FINAL)
+			e1:SetValue(0)
+			e1:SetReset(RESET_EVENT+0x1fe0000)
+			tc:RegisterEffect(e1)
+			tc=sg:GetNext()
+		end
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_PIERCE)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e2)
+	end
+end
