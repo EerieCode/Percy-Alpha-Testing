24ebddd38b978cc52b187d1170595194b81fa4c1
pyrQ <30827955+pyrQ@users.noreply.github.com>
2018-05-10 22:09:17 +0000

    Glife IDs fix
    Was wrong all over the place.

--- CP18-JP/c100227008.lua
+++ CP18-JP/c100227008.lua
@@ -8,9 +8,9 @@ function c100227008.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCost(c100227038.thcost)
-	e1:SetTarget(c100227038.thtg)
-	e1:SetOperation(c100227038.thop)
+	e1:SetCost(c100227008.thcost)
+	e1:SetTarget(c100227008.thtg)
+	e1:SetOperation(c100227008.thop)
 	c:RegisterEffect(e1)
 	--destroy
 	local e2=Effect.CreateEffect(c)
@@ -27,33 +27,33 @@ function c100227008.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 c100227038.listed_names={100227010}
-function c100227038.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100227008.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsDiscardable() end
 	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
 end
-function c100227038.thfilter(c)
+function c100227008.thfilter(c)
 	return c:IsCode(100227010) and c:IsAbleToHand()
 end
-function c100227038.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(c100227038.thfilter,tp,LOCATION_DECK,0,1,nil) end
+function c100227008.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100227008.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function c100227038.thop(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tg=Duel.GetFirstMatchingCard(c100227038.thfilter1,tp,LOCATION_DECK,0,nil)
+function c100227008.thop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetFirstMatchingCard(c100227008.thfilter1,tp,LOCATION_DECK,0,nil)
 	if tg then
 		Duel.SendtoHand(tg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tg)
 	end
 end
-function c100227038.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c100227008.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(1-tp) end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_SZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_SZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
-function c100227038.desop(e,tp,eg,ep,ev,re,r,rp)
+function c100227008.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
