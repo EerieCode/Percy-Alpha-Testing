7410952c7665f8c295548f7b3e37a4686dc7e411
Larry126 <larry126@users.noreply.github.com>
2018-05-14 16:56:00 +0100

    Fixed Talkback

--- SD34/c100334043.lua
+++ SD34/c100334043.lua
@@ -1,5 +1,6 @@
 --トークバック・ランサー
---Talkback Lancer 
+--Talkback Lancer
+--scripted by Larry126
 function c100334043.initial_effect(c)
 	--link summon
 	c:EnableReviveLimit()
@@ -11,7 +12,7 @@ function c100334043.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCost(c100334043.spcost)
+	e1:SetCountLimit(1,100334043)
 	e1:SetTarget(c100334043.sptg)
 	e1:SetOperation(c100334043.spop)
 	c:RegisterEffect(e1)
@@ -19,33 +20,27 @@ end
 function c100334043.matfilter(c,lc,sumtype,tp)
 	return c:IsLevelBelow(2) and c:IsRace(RACE_CYBERSE,lc,sumtype,tp)
 end
-function c100334043.spcfilter(c,g,zone)
-	return c:IsRace(RACE_CYBERSE) and (zone~=0 or g:IsContains(c))
-	and Duel.IsExistingMatchingCard(c64514622.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,zone,c:GetCode()) 
+function c100334043.spfilter(c,e,tp,zone,code)
+	if zone==0 then zone=0xff end
+	return c:IsSetCard(0x101) and c:GetOriginalCode()~=code
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone)
 end
-function c100334043.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100334043.spcfilter(c,g,zone,e)
+	return c:IsRace(RACE_CYBERSE) and (zone~=0 or g:IsContains(c))
+		and Duel.IsExistingTarget(c100334043.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,zone,c:GetOriginalCode()) 
+end
+function c100334043.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
 	local lg=c:GetLinkedGroup()
 	local zone=c:GetFreeLinkedZone()&0x1f
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,c100334043.spcfilter,1,false,nil,e:GetHandler(),lg,zone) end
-	local tc=Duel.SelectReleaseGroupCost(tp,c100334043.spcfilter,1,1,false,nil,e:GetHandler(),lg,zone)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100334043.spfilter(chkc,e,tp,zone) end
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,c100334043.spcfilter,1,false,nil,c,lg,zone,e) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local tc=Duel.SelectReleaseGroupCost(tp,c100334043.spcfilter,1,1,false,nil,c,lg,zone,e)
 	Duel.Release(tc,REASON_COST)
-	e:SetLabelObject(tc)
-end
-function c100334043.spfilter(c,e,tp,zone,code)
-	if not zone then zone=0xff end
-	return c:IsSetCard(0x101) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zone) and not c:GetOriginalCode()~=code
-end
-function c100334043.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local cc=e:GetLabelObject()
-	local zone=e:GetHandler():GetFreeLinkedZone()&0x1f
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and chkc~=cc
-		and c100334043.spfilter(chkc,e,tp,zone) or c100334043.spfilter(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(c100334043.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,zone,cc:GetOriginalCode())
-	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100334043.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,zone,cc:GetOriginalCode())
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	local g=Duel.SelectTarget(tp,c100334043.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,c:GetFreeLinkedZone()&0x1f,tc:GetFirst():GetOriginalCode())
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,LOCATION_GRAVE)
 end
 function c100334043.spop(e,tp,eg,ep,ev,re,r,rp)
 	local zone=e:GetHandler():GetFreeLinkedZone()&0x1f
