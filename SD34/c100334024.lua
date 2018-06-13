c126958989f347556f9aecc5c77481070241b786
EerieCode <eeriecode@gmail.com>
2018-06-13 17:55:10 +0200

    Updated "Cynet Codec"

--- SD34/c100334024.lua
+++ SD34/c100334024.lua
@@ -10,30 +10,19 @@ function c100334024.initial_effect(c)
 	--search
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_CUSTOM+100334024)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CHAIN_UNIQUE)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTarget(c100334024.thtg)
 	e2:SetOperation(c100334024.thop)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetLabelObject(e2)
-	e3:SetCondition(c100334024.regcon)
-	e3:SetOperation(c100334024.regop)
-	c:RegisterEffect(e3)
 	--register attributes
 	if not c100334024.global_flag then
 		c100334024.global_flag=true
 		c100334024.attr_list={}
 		c100334024.attr_list[0]=0
 		c100334024.attr_list[1]=0
-		c100334024.summon_group={}
-		c100334024.summon_group[0]=nil
-		c100334024.summon_group[1]=nil
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_PHASE+PHASE_END)
@@ -45,7 +34,7 @@ function c100334024.resetop(e,tp,eg,ep,ev,re,r,rp)
 	c100334024.attr_list[0]=0
 	c100334024.attr_list[1]=0
 end
-function c100334024.regfilter(c,e,tp)
+function c100334024.thcfilter(c,e,tp)
 	local attr=c:GetAttribute()
 	return c:IsSetCard(0x101) and c:IsControler(tp) 
 		and c:IsLocation(LOCATION_MZONE) and c:IsPreviousLocation(LOCATION_EXTRA)
@@ -56,45 +45,15 @@ end
 function c100334024.thfilter(c,attr)
 	return c:IsRace(RACE_CYBERSE) and c:IsAttribute(attr) and c:IsAbleToHand()
 end
-function c100334024.regcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.CheckChainUniqueness() and eg:IsExists(c100334024.regfilter,1,nil,e:GetLabelObject(),tp)
-end
-function c100334024.filter(c)
-	return c:IsFaceup() and c:IsCode(100334024) and not c:IsDisabled()
-end
-function c100334024.regop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(tp,100334024)==0 and Duel.SelectYesNo(tp,aux.Stringid(100334024,0)) then
-		Duel.RegisterFlagEffect(tp,100334024,RESET_CHAIN,0,1)
-		Debug.Message("Effect to be activated.")
-		local g=Duel.GetMatchingGroup(c100334024.filter,tp,LOCATION_SZONE,0,nil)
-		local c=nil
-		Debug.Message("Checking other Cynet Codecs.")
-		if #g==1 then
-			c=g:GetFirst()
-			Debug.Message("No other Cynet Codecs.")
-		else
-			Debug.Message("Other Cynet Codecs on field, must choose.")
-			c=g:Select(tp,1,1,nil):GetFirst()
-		end
-		local sg=eg:Filter(c100334024.regfilter,nil,e:GetLabelObject(),tp)
-		Debug.Message("#sg="..#sg)
-		sg:KeepAlive()
-		c100334024.summon_group[tp]=sg
-		if c100334024.summon_group[tp] then Debug.Message("#var="..c100334024.summon_group[tp]:GetCount()) else Debug.Message("Global variable not set.") end
-		Duel.RaiseSingleEvent(c,EVENT_CUSTOM+100334024,re,r,rp,0,0)
-	end
-end
 function c100334024.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local g=c100334024.summon_group[tp]
-	if chkc then return g:IsContains(chkc) and c100334024.regfilter(chkc,e,tp) end
-	if chk==0 then return true end
+	if chkc then return eg:IsContains(chkc) and c100334024.thcfilter(chkc,e,tp) end
+	if chk==0 then return eg:IsExists(c100334024.thcfilter,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local sg=g:FilterSelect(tp,c100334024.regfilter,1,1,nil,e,tp)
+	local sg=eg:FilterSelect(tp,c100334024.thcfilter,1,1,nil,e,tp)
 	Duel.SetTargetCard(sg)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function c100334024.thop(e,tp,eg,ep,ev,re,r,rp)
-	c100334024.summon_group[tp]=nil
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if not c:IsRelateToEffect(e) then return end
