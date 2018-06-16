9990f92c4b6dcb3d3f3a36ad80a02c9ccbaea3f8
edo9300 <edoardo762@gmail.com>
2018-06-16 16:23:47 +0200

    Added remaining effects for core radiator and micro coder

--- SD34/c100334004.lua
+++ SD34/c100334004.lua
@@ -12,6 +12,18 @@ function c100334004.initial_effect(c)
 	e1:SetOperation(c100334004.extracon)
 	e1:SetValue(c100334004.extraval)
 	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100334004,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCountLimit(1,100334004)
+	e2:SetCondition(c100334004.thcon)
+	e2:SetTarget(c100334004.thtg)
+	e2:SetOperation(c100334004.thop)
+	c:RegisterEffect(e2)
 end
 function c100334004.extracon(c,e,tp,sg,mg,lc,og,chk)
 	return (sg+mg):Filter(Card.IsLocation,nil,LOCATION_MZONE):IsExists(Card.IsRace,og,1,RACE_CYBERSE) and
@@ -38,7 +50,29 @@ function c100334004.extraval(chk,summon_type,e,...)
 	else
 		local sg,sc,tp=...
 		if summon_type==SUMMON_TYPE_LINK and #sg>0 then
+			Duel.Hint(HINT_CARD,tp,100334004)
 			Duel.RegisterFlagEffect(tp,100334004,RESET_PHASE+PHASE_END,0,1)
 		end
+	end
+end
+function c100334004.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	e:SetLabel(0)
+	if c:IsPreviousLocation(LOCATION_ONFIELD) then e:SetLabel(1) end
+	return c:IsLocation(LOCATION_GRAVE) and c:IsPreviousLocation(LOCATION_ONFIELD+LOCATION_HAND) and r==REASON_LINK and c:GetReasonCard():IsSetCard(0x101)
+end
+function c100334004.thfilter(c,chk)
+	return ((c:IsSetCard(0x218) and c:IsType(TYPE_SPELL+TYPE_TRAP)) or (chk==1 and c:IsRace(RACE_CYBERSE) and c:IsLevel(4))) and c:IsAbleToHand()
+end
+function c100334004.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100334004.thfilter,tp,LOCATION_DECK,0,1,nil,e:GetLabel()) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c100334004.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c100334004.thfilter,tp,LOCATION_DECK,0,1,1,nil,e:GetLabel())
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
--- SD34/c100334006.lua
+++ SD34/c100334006.lua
@@ -12,6 +12,18 @@ function c100334006.initial_effect(c)
 	e1:SetOperation(c100334006.extracon)
 	e1:SetValue(c100334006.extraval)
 	c:RegisterEffect(e1)
+	--disable
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(100334006,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCountLimit(1,100334006)
+	e2:SetCondition(c100334006.discon)
+	e2:SetTarget(c100334006.distg)
+	e2:SetOperation(c100334006.disop)
+	c:RegisterEffect(e2)
 end
 function c100334006.extracon(c,e,tp,sg,mg,lc,og,chk)
 	return (sg+mg):Filter(Card.IsLocation,nil,LOCATION_MZONE):IsExists(Card.IsRace,og,1,RACE_CYBERSE) and
@@ -38,7 +50,49 @@ function c100334006.extraval(chk,summon_type,e,...)
 	else
 		local sg,sc,tp=...
 		if summon_type==SUMMON_TYPE_LINK and #sg>0 then
+			Duel.Hint(HINT_CARD,tp,100334006)
 			Duel.RegisterFlagEffect(tp,100334006,RESET_PHASE+PHASE_END,0,1)
+		end
+	end
+end
+function c100334006.discon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	e:SetLabel(0)
+	if c:IsPreviousLocation(LOCATION_ONFIELD) then e:SetLabel(1) end
+	return c:IsLocation(LOCATION_GRAVE) and c:IsPreviousLocation(LOCATION_ONFIELD+LOCATION_HAND) and r==REASON_LINK and c:GetReasonCard():IsSetCard(0x101)
+end
+function c100334006.disfilter(c)
+	return c:IsFaceup() and not (c:GetAttack()==0 and c:IsDisabled())
+end
+function c100334006.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c100334006.disfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c100334006.disfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c100334006.disfilter,tp,0,LOCATION_MZONE,1,1+e:GetLabel(),nil)
+end
+function c100334006.disop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	for tc in aux.Next(g) do
+		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetValue(0)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2)
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_DISABLE_EFFECT)
+			e3:SetValue(RESET_TURN_SET)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e3)
 		end
 	end
 end
