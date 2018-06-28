3fb9f49001738f236d3a9bff15cfeabefbb2c54c
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-06-28 10:28:53 -0300

    Update c101006072.lua

--- SOFU-JP/c101006072.lua
+++ SOFU-JP/c101006072.lua
@@ -23,24 +23,26 @@ function c101006072.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c101006072.spfilter2(c,code,e,tp)
-	return c:GetCode()==code and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsCode(code) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c101006072.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.GetFirstTarget()
+	local code=tc:GetCode()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-		local code=tc:GetCode()
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 		local sg=Duel.GetMatchingGroup(c101006072.spfilter2,tp,LOCATION_GRAVE,0,nil,code,e,tp)
+		Debug.Message(#sg)
+		Debug.Message(ft)
+		if ft<=0 or sg:GetCount()==0 then return end
+		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 		if sg:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(101006072,0)) then
-			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local dg=sg:Select(tp,1,1,nil)
-			Duel.SpecialSummon(dg,0,tp,tp,false,false,POS_FACEUP)
-		end
-		dg:Merge(tc)
-		for dg in aux.Next(dg) do
+		local g=sg:Select(tp,ft,ft,nil)
+		local dg=g:GetFirst()
+		while dg do
+		Debug.Message(dg)
+		Duel.SpecialSummonStep(dg,0,tp,tp,false,false,POS_FACEUP)
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
@@ -57,6 +59,9 @@ function c101006072.activate(e,tp,eg,ep,ev,re,r,rp)
 			e2:SetTarget(c101006072.splimit)
 			e2:SetReset(RESET_EVENT+0x1fe0000)
 			dg:RegisterEffect(e2,true)
+			dg=g:GetNext()
+		end
+		Duel.SpecialSummonComplete()
 		end
 	end
 end
