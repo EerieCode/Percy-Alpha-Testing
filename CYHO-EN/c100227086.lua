12317e5ebcf178cee7cfb429db9997e8e80a6019
AlphaKretin <deewhydeeecks@gmail.com>
2018-07-03 13:13:48 +0930

    Complete "Realm of Danger!"

--- CYHO-EN/c100227086.lua
+++ CYHO-EN/c100227086.lua
@@ -16,7 +16,44 @@ function c100227086.intial_effect(c)
 	e2:SetTarget(c100227086.target)
 	e2:SetValue(aux.tgoval)
 	c:RegisterEffect(e2)
+	--direct attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetOperation(c100227086.daop)
+	c:RegisterEffect(e3)
 end
 function c100227086.target(e,c)
 	return c:IsSetCard(0x233) and c:IsStatus(STATUS_SPSUMMON_TURN)
+end
+function c100227086.dafilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x233)
+end
+function c100227086.datg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c100227086.dafilter(chkc)
+	if chk==0 return Duel.IsExistingTarget(c100227086.dafilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,0,1,1,c)
+end
+function c100227086.daop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not (c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup()) then return end
+	c:SetCardTarget(tc)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_SINGLE_RANGE))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetConditon(c100227086.rcon)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
+	c:RegisterEffect(e2)
+end
+function c100227086.rcon(e)
+	return e:GetOwner():IsHasCardTarget(e:GetHandler())
 end
