13d8b7982fa4bce29fa43e060632619dd779dc2c
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-07-13 15:01:09 -0300

    Update c101006066.lua
    small adjust (i think i overcomplicated the effect somehow)

--- SOFU-JP/c101006066.lua
+++ SOFU-JP/c101006066.lua
@@ -1,4 +1,5 @@
 --Parallel Panzer
+--scripted by andré
 function c101006066.initial_effect(c)
 	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsType,TYPE_LINK))
 	--move to a zone it point
@@ -19,6 +20,13 @@ function c101006066.initial_effect(c)
 	e2:SetTarget(c101006066.dtarget)
 	e2:SetOperation(c101006066.doperation)
 	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_LEAVE_FIELD_P)
+	e3:SetOperation(c101006066.checkop)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
 end
 function c101006066.mtarget(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
@@ -48,23 +56,65 @@ function c101006066.dcondition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:GetPreviousLocation()==LOCATION_SZONE and not c:IsReason(REASON_LOST_TARGET)
 end
-function c101006066.filter(c,g)
-	return g:IsContains(c)
+function c101006066.checkop(e,tp)
+	local c=e:GetHandler()
+	local ec=c:GetEquipTarget()
+	if ec then
+		e:GetLabelObject():SetLabel(ec:GetControler()|(ec:GetSequence()<<2))
+	else
+		e:GetLabelObject():SetLabel(-1)
+	end
+end
+function c101006066.filter(c,tp,seq)
+	local cseq=c:GetSequence()
+	local cp=c:GetControler()
+	if cp==tp then
+		if c:IsLocation(LOCATION_MZONE) then
+			if seq==1 or seq==5 then
+				return cseq==1 or cseq==5
+			elseif seq==3 or seq==6 then
+				return cseq==3 or cseq==6
+			else
+				return cseq==seq
+			end
+		else
+			if seq==5 or seq==6 then
+				return (cseq==1 and seq==5) or (cseq==3 and seq==6)
+			else
+				return cseq==seq
+			end
+		end
+	else
+		if c:IsLocation(LOCATION_MZONE) then
+			if seq==1 or seq==5 then
+				return cseq==3 or cseq==6
+			elseif seq==3 or seq==6 then
+				return cseq==1 or cseq==5
+			else
+				return cseq==4-seq
+			end
+		else
+			if seq==5 or seq==6 then
+				return (cseq==1 and seq==6) or (cseq==3 and seq==5)
+			else
+				return cseq==4-seq
+			end
+		end
+	end
 end
 function c101006066.dtarget(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local ec=c:GetPreviousEquipTarget()
-	if chk==0 then
-		if not ec then return false end
-		e:SetLabelObject(ec)
-		return Duel.IsExistingMatchingCard(c101006066.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil,ec:GetColumnGroup())
-	end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,ec:GetColumnGroup(),1,0,0)
+	local cp=e:GetLabel()&0x3
+	local seq=e:GetLabel()>>2
+	local g=Duel.GetMatchingGroup(c101006066.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,cp,seq)
+	if chk==0 then return seq>=0 and #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c101006066.doperation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local ec=e:GetLabelObject()
+	local cp=e:GetLabel()&0x3
+	local seq=e:GetLabel()>>2
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local dg=Duel.SelectMatchingCard(tp,c101006066.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,ec:GetColumnGroup())
+	local dg=Duel.SelectMatchingCard(tp,c101006066.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil,cp,seq)
 	Duel.Destroy(dg,REASON_EFFECT)
 end
