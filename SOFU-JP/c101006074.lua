a7afe932bdd7740535c5dc2d760264a29ce425d1
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-07-13 13:25:31 +0200

    Added Krawler Palladion

---
+++ SOFU-JP/c101006074.lua
@@ -0,0 +1,59 @@
+--クローラー・パラディオン
+-- Krawler Palladion
+function c101006074.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c101006074.target)
+	e1:SetOperation(c101006074.activate)
+	c:RegisterEffect(e1)
+end
+function c101006074.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,101006074,0x01040116,0x21,300,2100,2,RACE_INSECT,ATTRIBUTE_EARTH)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c101006074.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,101006074,0x01040116,0x21,300,2100,2,RACE_INSECT,ATTRIBUTE_EARTH) then return end
+	c:AddMonsterAttribute(TYPE_EFFECT+TYPE_TRAP)
+	Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
+	c:AddMonsterAttributeComplete()
+	--monster effects
+	--to hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(101006074,0))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCountLimit(1,101006074)
+	e3:SetTarget(c101006074.thtg)
+	e3:SetOperation(c101006074.thop)
+	e3:SetReset(RESET_EVENT+0x1fe0000)
+	c:RegisterEffect(e3,true)
+	Duel.SpecialSummonComplete()
+end
+function c29088922.cfilter(c,tc)
+	return c:IsFaceup() and c:GetLinkedGroup():IsContains(tc)
+end
+function c101006074.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c29088922.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c101006074.filter(c)
+	return c:IsAttackBelow(1500) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c101006074.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c101006074.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
