10811c193ea126e985f855b2e38e66c241f037ca
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-07-17 14:20:10 +0200

    Update c100410014.lua

--- DBHS-JP/c100410014.lua
+++ DBHS-JP/c100410014.lua
@@ -6,17 +6,18 @@ function c100410014.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BE_MATERIAL)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e1:SetCondition(c100410014.indcon)
-	e1:SetOperation(c100410014.indop)
+	e1:SetCondition(c100410014.tgcon)
+	e1:SetTarget(c100410014.tgtg)
+	e1:SetOperation(c100410014.tgop)
 	c:RegisterEffect(e1)
 end
-function c100410014.ddcon(e,tp,eg,ep,ev,re,r,rp)
+function c100410014.tgcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local rc=c:GetReasonCard()
 	return c:IsLocation(LOCATION_GRAVE) and ((r & REASON_LINK == REASON_LINK) or (r & REASON_FUSION == REASON_FUSION))
 		and rc:IsSetCard(0x226) and (rc:IsType(TYPE_LINK) or rc:IsType(TYPE_FUSION))
 end
-function c100410014.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c100410014.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
 end
@@ -26,7 +27,7 @@ end
 function c100410014.spfilter(c,e,tp)
 	return c:IsSetCard(0x226) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)  and not c:IsCode(100410014)
 end
-function c100410014.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c100410014.tgop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,c100410014.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
 	local g2=Duel.GetMatchingGroup(c100410014.spfilter,tp,LOCATION_DECK,0,g:GetFirst(),e,tp)
