8c77a4115d23bc8bc233dc3add6fe53d4b0ca4b4
EerieCode <eeriecode@gmail.com>
2018-07-17 16:08:57 +0200

    Fixed "Prankids Rocket"

--- DBHS-JP/c100410017.lua
+++ DBHS-JP/c100410017.lua
@@ -31,7 +31,7 @@ function c100410017.atkcon(e)
 end
 function c100410017.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
+	if not c:IsRelateToEffect(e) or c:GetAttack()<1000 then return end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
@@ -58,8 +58,9 @@ end
 function c100410017.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	local g=Duel.GetMatchingGroup(c100410017.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+	if chk==0 then return ft>1 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
 		and aux.SelectUnselectGroup(g,e,tp,2,2,c100410017.spcheck,0) end
 	local tg=aux.SelectUnselectGroup(g,e,tp,2,2,c100410017.spcheck,1,tp,HINTMSG_SPSUMMON)
 	Duel.SetTargetCard(tg)
