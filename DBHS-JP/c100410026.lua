a31b2d6dcdd9539a1e94a102f075275e99ba4611
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-07-26 07:26:16 -0300

    Fixed "Prankids Super Scheme"

--- DBHS-JP/c100410026.lua
+++ DBHS-JP/c100410026.lua
@@ -3,7 +3,7 @@
 --Scripted by Eerie Code
 function c100410026.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_EFFECT)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(c100410026.target)
 	c:RegisterEffect(e1)
