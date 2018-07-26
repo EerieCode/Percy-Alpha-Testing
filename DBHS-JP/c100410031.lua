c1a0129d7c068d55a0997fa64e4a46fe45e66abe
AlphaKretin <deewhydeeecks@gmail.com>
2018-07-26 17:36:05 +0930

    Fix "Skeleton Mayakashi - Yasha"
    Name and activated summon

--- DBHS-JP/c100410031.lua
+++ DBHS-JP/c100410031.lua
@@ -1,15 +1,16 @@
--- 
--- Mayakashi
+--骸魔妖ー夜叉 
+--Skeleton Mayakashi - Yasha
 --Scripted by AlphaKretin
 function c100410031.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetDescription(aux.Stringid(100410031,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,100410031)
-	e1:SetCondition(c100410031.spcon)
+	e1:SetCost(c100410031.spcost)
+	e1:SetTarget(c100410031.sptg)
 	e1:SetOperation(c100410031.spop)
 	c:RegisterEffect(e1)
 	--spsummon limit
@@ -22,20 +23,23 @@ function c100410031.initial_effect(c)
 	e2:SetTarget(c100410031.sslimit)
 	c:RegisterEffect(e2)
 end
-
-function c100410031.spfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x227)
+function c100410031.cfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x227) and c:IsDiscardable()
 end
-function c100410031.spcon(e,c)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c100410031.spfilter,tp,LOCATION_HAND,0,1,c)
+function c100410031.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c100410031.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,c100410031.cfilter,1,1,REASON_COST+REASON_DISCARD,e:GetHandler())
 end
-function c100410031.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-	local g=Duel.SelectMatchingCard(tp,c100410031.spfilter,tp,LOCATION_HAND,0,1,1,c)
-	Duel.SendtoGrave(g,REASON_COST+REASON_DISCARD)
+function c100410031.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c100410031.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
 function c100410031.sslimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return c:IsLocation(LOCATION_EXTRA) and not c:IsSetCard(0x227)
