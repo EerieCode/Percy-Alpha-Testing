593e5b51ab6d9873f5d71898686c9ac377d3daa4
AlphaKretin <deewhydeeecks@gmail.com>
2018-08-03 13:45:52 +0930

    Update "Ride of the Valkyries"

--- SHVA-EN/cXXXXXX006.lua
+++ SHVA-EN/cXXXXXX006.lua
@@ -1,27 +1,26 @@
 --Ride of the Valkyries
---scripted by Naim
+--scripted by Naim and AlphaKretin
 --find replace the below values when revealed
 --scripted parts 1, 2 and 3, missing 4
-local CARD;
-local CODE;
-local SET_VALKYRIE;
+local CARD
+local CODE
+local SET_VALKYRIE
+local MISCHIEF_CODE_HERE
 function CARD.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	--e1:SetCountLimit(1,14517422+EFFECT_COUNT_CODE_OATH)		--if it is once per turn or something
 	e1:SetTarget(CARD.sptg)
 	e1:SetOperation(CARD.spop)
 	c:RegisterEffect(e1)
-		--to hand
+	--to hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(CODE,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	--e2:SetCondition(aux.exccon)		--except the turn this card was sent to the Graveyard
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(CARD.thtg)
 	e2:SetOperation(CARD.thop)
@@ -42,30 +41,32 @@ function CARD.spop(e,tp,eg,ep,ev,re,r,rp)
 	if ft>5 then ft=5 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,CARD.filter,tp,LOCATION_HAND,0,1,ft,nil,e,tp)
-	if g:GetCount()>0 then
-		local tc=g:GetFirst()
-			while tc do
-			Duel.SpecialSummonStep(tc,0,tp,tp,false,false)
-			local e2=Effect.CreateEffect(c)
+	if #g>0 then
+		local ct=Duel.SpecialSummon(g,0,tp,tp,false,false)
+		if ct>2 then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_FIELD)
+			e1:SetCode(EFFECT_CHANGE_DAMAGE)
+			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+			e1:SetTargetRange(1,0)
+			e1:SetValue(0)
+			e1:SetReset(RESET_PHASE+PHASE_END,2)
+			Duel.RegisterEffect(e1,tp)
+		end
+	end
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+			local e2=Effect.CreateEffect(e:GetHandler())
 			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 			e2:SetCode(EVENT_PHASE+PHASE_END)
-			e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-			e2:SetRange(LOCATION_MZONE)
 			e2:SetCountLimit(1)
-			e2:SetCondition(CARD.tdcon)
+			e2:SetReset(RESET_PHASE+PHASE_END)
 			e2:SetOperation(CARD.tdop)
-			e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e2,true)
-			tc=g:GetNext()
-			end
-		Duel.SpecialSummonComplete()
-	end
-end
-function CARD.tdcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
+			Duel.RegisterEffect(e2,tp)
+		end
 end
 function CARD.tdop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SendtoDeck(e:GetHandler(),nil,2,REASON_EFFECT)
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	Duel.SendtoDeck(g,tp,2,REASON_EFFECT)
 end
 function CARD.thfilter(c)
 	return c:IsCode(MISCHIEF_CODE_HERE) and c:IsAbleToHand()
@@ -76,8 +77,8 @@ function CARD.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function CARD.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,CARD.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
+	local g=Duel.GetFirstMatchingCard(tp,CARD.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
