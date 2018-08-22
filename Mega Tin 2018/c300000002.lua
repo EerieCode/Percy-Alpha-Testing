c277ed163a1d4d1cb06011f3f56c60d0c94d8fcd
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-08-23 01:51:02 +0200

    Junk Speeder Fix
    forgot the double atk was only the turn it was synchro summoned + renaming for the special summon effect target and operation

--- Mega Tin 2018/c300000002.lua
+++ Mega Tin 2018/c300000002.lua
@@ -15,16 +15,24 @@ function c300000002.initial_effect(c)
 	e1:SetTarget(c300000002.sptg)
 	e1:SetOperation(c300000002.spop)
 	c:RegisterEffect(e1)
-	--atk u/d
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(300000102,0))
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,300000102)
-	e1:SetCondition(c300000002.atkcon)
-	e1:SetOperation(c300000002.atkop)
-	c:RegisterEffect(e1,false,1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(300000102,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,300000102)
+	e2:SetCondition(c300000002.atkcon)
+	e2:SetOperation(c300000002.atkop)
+	c:RegisterEffect(e2,false,1)
+	--extra attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCondition(c300000002.effcon)
+	e3:SetOperation(c300000002.regop)
+	c:RegisterEffect(e3)
 	Duel.AddCustomActivityCounter(300000002,ACTIVITY_SPSUMMON,c300000002.counterfilter)
 end
 function c300000002.counterfilter(c)
@@ -50,14 +58,14 @@ end
 function c300000002.filter(c,e,tp)
 	return c:IsSetCard(0x1017) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c300000002.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c300000002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local ct=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD)
 		return ct>0 and Duel.IsExistingMatchingCard(c300000002.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
-function c300000002.activate(e,tp,eg,ep,ev,re,r,rp)
+function c300000002.spop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD)
 	if ct<=0 then return end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ct=1 end
@@ -74,10 +82,15 @@ function c300000002.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP,zone)
 	end
 end
-
+function c300000002.effcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+end
+function c300000002.regop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(300000002,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+end
 
 function c300000002.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return (e:GetHandler()==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil) or e:GetHandler()==Duel.GetAttackTarget()
+	return e:GetHandler():GetFlagEffect(300000002)~=0 and (e:GetHandler()==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil) or e:GetHandler()==Duel.GetAttackTarget()
 end
 function c300000002.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
