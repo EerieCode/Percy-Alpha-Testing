b08bd6baa83f1e62bb9401185fcdd7ad14e8fe67
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-08-23 01:52:09 +0200

    Update c300000002.lua
    forgot the IsType Tuner in filter

--- Mega Tin 2018/c300000002.lua
+++ Mega Tin 2018/c300000002.lua
@@ -56,7 +56,7 @@ function c300000002.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
 end
 function c300000002.filter(c,e,tp)
-	return c:IsSetCard(0x1017) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x1017) and c:IsType(TYPE_TUNER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c300000002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
