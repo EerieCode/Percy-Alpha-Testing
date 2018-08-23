2361c927d1635b9b0f579b25c73d1724ec801f66
AlphaKretin <deewhydeeecks@gmail.com>
2018-08-23 17:20:12 +0930

    Update "Junk Speeder"
    SelectUnselect

--- Mega Tin 2018/c300000002.lua
+++ Mega Tin 2018/c300000002.lua
@@ -65,21 +65,19 @@ function c300000002.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
+function c300000002.rescon(sg,e,tp,mg)
+    return aux.ChkfMMZ(#sg)(sg, e, tp, mg) and sg:GetClassCount(Card.GetLevel) == 1
+end
 function c300000002.spop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD)
 	if ct<=0 then return end
 	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ct=1 end
 	local g=Duel.GetMatchingGroup(c300000002.filter,tp,LOCATION_DECK,0,nil,e,tp)
-	if g:GetCount()>0 then
-		local sg=Group.CreateGroup()
-		repeat
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local sg2=g:Select(tp,1,1,nil)
-			sg:AddCard(sg2:GetFirst())
-			g:Remove(Card.IsLevel,nil,sg2:GetFirst():GetLevel())
-			ct=ct-1
-		until ct==0 or g:GetCount()==0 or not Duel.SelectYesNo(tp,aux.Stringid(300000002,0))
-		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP,zone)
+	if #g>0 then
+		 local sg=aux.SelectUnselectGroup(g,e,tp,1,ct,c300000002.rescon,1,tp,HINTMSG_SPSUMMON)
+		 if #sg>0 then
+		 	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		 end
 	end
 end
 function c300000002.effcon(e,tp,eg,ep,ev,re,r,rp)
