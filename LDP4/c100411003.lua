45597c74bbe850bc073d39a8a6f6b45ae555d9be
ClementLouis <clementlouis.gallier@viacesi.fr>
2018-09-15 12:37:43 +0200

    Update c100411003.lua

--- LDP4/c100411003.lua
+++ LDP4/c100411003.lua
@@ -1,5 +1,5 @@
 --Split Mirrors of Seduction
-function c712559.initial_effect(c)
+function c100411003.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
