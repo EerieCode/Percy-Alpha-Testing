938adea35a8470ed96cdaeafb498e6a43373b093
Larry126 <larry126@users.noreply.github.com>
2018-09-22 18:45:42 +0100

    Fixes

--- Legendary Hero Decks/c100234001.lua
+++ Legendary Hero Decks/c100234001.lua
@@ -4,15 +4,15 @@
 -- in your GY. If this card attacks a Defense Position monster, inflict piercing battle damage.
 function c100234001.initial_effect(c)
 	--link summon
-	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkSetCard,0x8),2)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsLinkSetCard,0x8),2)
 	c:EnableReviveLimit()
-	--atk/def
+	--atk
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e1:SetTarget(c100234001.tgtg)
+	e1:SetTarget(c100234001.atktg)
 	e1:SetValue(c100234001.atkval)
 	c:RegisterEffect(e1)
 	--pierce
@@ -21,11 +21,9 @@ function c100234001.initial_effect(c)
 	e2:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e2)
 end
-function c100234001.tgtg(e,c)
-	return (e:GetHandler():GetLinkedGroup():IsContains(c) or c==e:GetHandler()) and c:IsSetCard(0x8)
+function c100234001.atktg(e,c)
+	return (e:GetHandler():GetLinkedGroup():IsContains(c) or c==e:GetHandler()) and c:IsSetCard(0x8) and c:IsFaceup()
 end
 function c100234001.atkval(e,c)
-	local g=Duel.GetMatchingGroup(Card.IsSetCard,c:GetControler(),LOCATION_GRAVE,0,nil,0x8)
-	local ct=g:GetClassCount(Card.GetCode)
-	return ct*100
+	return Duel.GetMatchingGroup(Card.IsSetCard,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,0x8):GetClassCount(Card.GetCode)*100
 end
