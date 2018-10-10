daad0009e69076aa7d53cf6fa05a2687982e2267
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-10 08:15:50 -0300

    Missing comma

--- SAST-JP/c101007055.lua
+++ SAST-JP/c101007055.lua
@@ -30,7 +30,7 @@ function s.natg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local sg=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE1,1,nil)
+	local sg=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
 end
 function s.naop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
