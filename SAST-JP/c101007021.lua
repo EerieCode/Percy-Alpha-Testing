53e20f142f521c6e894521c86b5ab52141cf4edd
AlphaKretin <deewhydeeecks@gmail.com>
2018-10-12 23:32:44 +1030

    intial
    this one was mine orz

--- SAST-JP/c101007021.lua
+++ SAST-JP/c101007021.lua
@@ -2,7 +2,7 @@
 --Orcustrated Knightmare
 --Scripted by AlphaKretin
 local s,id=GetID()
-function s.intial_effect(c)
+function s.initial_effect(c)
 	--indes
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
