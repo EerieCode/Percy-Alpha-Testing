1c01604bb9463a35e977c79695c1e602e4b95938
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-12 09:57:12 -0300

    Update c101007068.lua

--- SAST-JP/c101007068.lua
+++ SAST-JP/c101007068.lua
@@ -1,5 +1,5 @@
 --土俵間際
---Edge of the Ring
+--On the Edge of the Ring
 --Scripted by ahtelel
 local s,id=GetID()
 function s.initial_effect(c)
@@ -21,10 +21,10 @@ function s.filter(c)
 	return c:IsFaceup() and c:GetBaseAttack()>0 
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetFirst():GetBaseAttack()/2)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
