9736e8d8e17e16ee6857a77a650b071608432a90
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-12 10:40:32 -0300

    Many fixes for "Unizong Tune"

--- SAST-JP/c101007066.lua
+++ SAST-JP/c101007066.lua
@@ -1,55 +1,54 @@
+--
 --Unizong Tune
---Logical Nonsense
-
---Substitute ID
+--scripted Logical Nonsense
 local s,id=GetID()
-
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(77994337,0))
-	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetRange(LOCATION_PZONE)
-	e1:SetCountLimit(1)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.lvtg)
 	e1:SetOperation(s.lvop)
 	c:RegisterEffect(e1)
 end
-	--Check for tuner monster that can be banished
 function s.filter(c)
 	local lv=c:GetLevel()
 	return c:IsType(TYPE_TUNER) and c:IsAbleToRemove() and c:GetLevel()>0 
 end
-	--Check for monster with a level on the field
 function s.lvfilter(c)
 	return c:IsFaceup() and c:GetLevel()>0
 end
-	--Activation legality
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.lvfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) 
-		and Duel.IsExistingTarget(c2204140.filter,tp,0,LOCATION_GRAVE,LOCATION_GRAVE,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.lvfilter(chkc,lv) end
+	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,0,1,nil) 
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g1=Duel.SelectTarget(tp,c2204140.filter,tp,0,LOCATION_GRAVE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g2,1,1,nil)
+	local g1=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,LOCATION_GRAVE,1,1,nil)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g1,1,1,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g2=Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	g1:Merge(g2)
 end
-	--Perfomr the effect of changing level and treatment as tuner
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)~=0 then
+	local tc1=e:GetLabelObject()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tc2=g:GetFirst()
+	if tc1==tc2 then tc2=g:GetNext() end
+	if tc1:IsRelateToEffect(e) and Duel.Remove(tc1,POS_FACEUP,REASON_EFFECT)~=0 then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CHANGE_LEVEL)
-		e1:SetValue(lv)
+		e1:SetCode(EFFECT_CHANGE_LEVEL_FINAL)
+		e1:SetValue(tc1:GetLevel())
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-		local e2=e1:Clone()
+		tc2:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_ADD_TYPE)
-		e2:SetCode(TYPE_TUNER)
-		tc:RegisterEffect(e2)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e2:SetValue(TYPE_TUNER)
+		tc2:RegisterEffect(e2)
 	end
 end
