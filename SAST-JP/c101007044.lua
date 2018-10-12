6dbdb6ac0f47a158d7456e9706cba36e61351757
Larry126 <larry126@users.noreply.github.com>
2018-10-12 06:36:08 +0100

    Remove battle protection and updates filter

--- SAST-JP/c101007044.lua
+++ SAST-JP/c101007044.lua
@@ -22,33 +22,29 @@ function s.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	c:RegisterEffect(e2)
-	--Cyberse monsters cannot be destroyed by battle
+	--Cyberse monsters cannot be destroyed by card effect
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e3:SetTarget(s.indtg)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
-	--Cyberse monsters cannot be destroyed by card effect
-	local e4=e3:Clone()
-	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	c:RegisterEffect(e4)
 	--Search Cyberse tuner, ignition effect
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCountLimit(1,id)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(s.thcon)
-	e1:SetCost(s.thcost)
-	e1:SetTarget(s.thtg)
-	e1:SetOperation(s.thop)
-	c:RegisterEffect(e1)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCountLimit(1,id)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.thcon)
+	e4:SetCost(s.thcost)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
 end
 	--Check if it was link summoned
 function s.indcon(e)
@@ -60,16 +56,12 @@ function s.indtg(e,c)
 	return c:IsRace(RACE_CYBERSE) and e:GetHandler():GetLinkedGroup():IsContains(c)
 end
 	--If a monster is special summoned to its zones
-function s.thcfilter(c,ec)
-	if c:IsLocation(LOCATION_MZONE) then
-		return ec:GetLinkedGroup():IsContains(c)
-	else
-		return bit.extract(ec:GetLinkedZone(c:GetPreviousControler()),c:GetPreviousSequence())~=0
-	end
+function s.thcfilter(c,tp,lg)
+	return c:IsControler(tp) and lg:IsContains(c)
 end
-	--If it ever happened
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.thcfilter,1,nil,e:GetHandler())
+	local lg=e:GetHandler():GetLinkedGroup()
+	return eg:IsExists(s.thcfilter,1,nil,tp,lg)
 end
 	--Check for Cyberse monster
 function s.cfilter(c)
@@ -95,7 +87,7 @@ end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
-	if g:GetCount()>0 then
+	if #g>0 then
 		Duel.SendtoHand(g1,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
