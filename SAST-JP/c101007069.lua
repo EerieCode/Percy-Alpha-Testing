088c17b8b41baf824e26b15253edd7de7c4e76af
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-10-11 22:49:59 -0300

    Update c101007069.lua

--- SAST-JP/c101007069.lua
+++ SAST-JP/c101007069.lua
@@ -1,5 +1,4 @@
 --Boldly Invincible
---scripted by ???
 local s,id=GetID()
 function s.initial_effect(c)
 	--activate
@@ -11,12 +10,14 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCondition(s.hcondition)
 	e2:SetOperation(s.hoperation)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
-	e2:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	e3:SetCondition(s.hcondition2)
 	c:RegisterEffect(e3)
 	local e4=e2:Clone()
 	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -31,12 +32,16 @@ function s.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 function s.filter(c,tp)
-	return c:GetSummonPlayer()~=tp
+	return c:GetSummonPlayer()==tp
 end
 function s.hcondition(e,tp,eg,ep,ev,re,r,rp)
-	return eg and eg:IsExists(c101007069.filter,1,nil,tp)
+	return eg and eg:IsExists(c101007069.filter,1,nil,1-tp)
+end
+function s.hcondition2(e,tp,eg,ep,ev,re,r,rp)
+	return eg and eg:IsExists(Card.IsControler,1,nil,1-tp)
 end
 function s.hoperation(e,tp)
+	Duel.Hint(HINT_CARD,0,id)
 	Duel.Recover(tp,300,REASON_EFFECT)
 end
 function s.bpcondition(e)
