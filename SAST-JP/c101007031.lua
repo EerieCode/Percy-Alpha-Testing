b968bb56fccee478040c97bb0b1a9fae3cc63c03
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-12 14:15:44 -0300

    Fixed & improved "Extracaretops"

--- SAST-JP/c101007031.lua
+++ SAST-JP/c101007031.lua
@@ -1,59 +1,21 @@
 --エキストラケアトップス
 --Extracaretops
---Scripted by AlphaKretin
+--Scripted by AlphaKretin and Andre
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
-	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetRange(LOCATION_GRAVE)
-	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-end
-function s.cfilter(c,tp)
-	local rc=c:GetReasonCard()
-	return c:IsReason(REASON_BATTLE) and c:IsLocation(LOCATION_GRAVE) and c:GetPreviousSequence()>4 
-		and rc and rc:GetSequence()<5
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	local rc=eg:Filter(s.cfilter,nil):GetFirst() --we know this exists because the condition is true
-	local op=rc:GetOwner()
-	e:SetLabel(p)
-	if chk==0 then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetCode(EFFECT_FORCE_MZONE)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e1:SetTargetRange(1,0)
-		e1:SetValue(0x60)
-		Duel.RegisterEffect(e1,tp)
-		local res=Duel.GetLocationCountFromEx(op,tp,c)>0
-			and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		e1:Reset()
-		return res
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_GRAVE)
-end
-function s.spop1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local op=e:GetLabel()
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_FORCE_MZONE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetValue(0x60)
-	Duel.RegisterEffect(e1,tp)
-	Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP)
 	--draw
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
@@ -64,13 +26,34 @@ function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetCondition(s.drcon)
 	e2:SetTarget(s.drtg)
 	e2:SetOperation(s.drop)
-	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-	c:RegisterEffect(e2)
-	Duel.SpecialSummonComplete()
-	e1:Reset()
+	c:RegisterEffect(e2)	
+end
+function s.cfilter(c)
+	local rc=c:GetReasonCard()
+	return c:IsReason(REASON_BATTLE) and c:GetPreviousSequence()>4 
+		and rc and ((rc:IsLocation(LOCATION_MZONE) and rc:GetSequence()<5) or (not rc:IsLocation(LOCATION_MZONE) and rc:GetPreviousSequence()<5))
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		local rc=eg:Filter(s.cfilter,nil):GetFirst()
+		local op=rc and rc:GetOwner() or nil
+		e:SetLabel(op)
+		return rc and c:IsCanBeSpecialSummoned(e,1,tp,false,false,POS_FACEUP_DEFENSE,op)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local op=e:GetLabel()
+	Duel.SpecialSummon(c,1,tp,op,false,false,POS_FACEUP_DEFENSE)
 end
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsReason(REASON_DESTROY)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:IsSummonType(SUMMON_TYPE_SPECIAL+1)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
@@ -79,5 +62,6 @@ function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
 end
