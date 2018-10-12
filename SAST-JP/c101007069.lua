a7c2b8124621897fbff6c812e7ef45840cd1bfed
AndreOliveiraMendes <ao_mendes@hotmail.com>
2018-10-11 22:12:00 -0300

    Update c101007069.lua

--- SAST-JP/c101007069.lua
+++ SAST-JP/c101007069.lua
@@ -11,6 +11,7 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetRange(LOCATION_SZONE)
 	e2:SetCondition(s.hcondition)
 	e2:SetOperation(s.hoperation)
 	c:RegisterEffect(e2)
@@ -25,6 +26,7 @@ function s.initial_effect(c)
 	e5:SetType(EFFECT_TYPE_FIELD)
 	e5:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e5:SetTargetRange(LOCATION_MZONE,0)
+	e5:SetRange(LOCATION_SZONE)
 	e5:SetCondition(s.bpcondition)
 	c:RegisterEffect(e5)
 end
@@ -34,6 +36,6 @@ end
 function s.hcondition(e,tp,eg,ep,ev,re,r,rp)
 	return eg and eg:IsExists(c101007069.filter,1,nil,tp)
 end
-function s.bpcondition(e,tp)
-	return Duel.GetLP(tp)>=10000
+function s.bpcondition(e)
+	return Duel.GetLP(e:GetHandlerPlayer())>=10000
 end
