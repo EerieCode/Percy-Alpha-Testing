b5145f71300221fc53147d31fe1feaff765826e4
ahtelel <32773013+ahtelel@users.noreply.github.com>
2018-10-15 11:38:52 +0700

    Added Machine Angel Manifestation

---
+++ DP21-JP/c100411015.lua
@@ -0,0 +1,56 @@
+--応身の機械天使
+--Machne Angel Manifestation
+--Scripted by ahtelel
+local s, id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--battle indestructable
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.infilter)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--ritual summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetCode(EVENT_DAMAGE)
+	e3:SetCountLimit(1,id)
+	e3:SetCost(s.spcost)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+end
+function s.infilter(e,c)
+	return c:IsRitualMonster() and c:IsSetCard(0x2093)
+end
+function s.spfilter(e,c)
+	return s.infilter(e,c) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,true,false)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.infilter,1,true,aux.ReleaseCheckTarget,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.infilter,1,1,true,aux.ReleaseCheckTarget,nil)
+	Duel.Release(g,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp):GetFirst()
+	if g:GetCount()>0 and Duel.SpecialSummon(g,SUMMON_TYPE_RITUAL,tp,tp,true,false,POS_FACEUP) then
+		g:CompleteProcedure()
+	end
+end
