40cbc014274b215d630c50e39f872807c6e8e342
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2018-10-16 03:27:06 -0400

    c100235026
    Uploaded "Alien Shocktrooper Mighty/Frame".
    
    Not completed, but most of the structure is laid out (first effect is covered mostly, second effect still needs to be worked on in terms of spop). Thought it would be best leaving it here, as I must leave for an unknown period of time while having no access to my workspace. I thank you in advance, whoever willingly decides to continue on this script (if so).

---
+++ Link Vrains Pack 2/c100235026.lua
@@ -0,0 +1,112 @@
+--エーリアン・ソルジャー M/フレーム
+--Alien Shocktrooper Mighty/Frame
+--Logical Nonsense
+
+--Substitute ID
+local s,id=GetID()
+
+function s.initial_effect(c)
+	--Discard to place A-counters, quick-play effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--If destroyed, reptile Soul Charge, trigger effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,id+100)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+end
+	--Defining cost
+function s.costfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsDiscardable() and c:GetOriginalLevel()>0
+end
+	--Cost of discarding a monster card with a level
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD)
+end
+	--Activation legality
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,g,1,0x100e,1)
+end
+	--Performing the effect of placing counters
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local lv=tc:GetLevel()
+		local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+		if g:GetCount()==0 then return end
+		for i=1,lv do
+			local sg=g:Select(tp,1,1,nil)
+			sg:GetFirst():AddCounter(0x100e,1)
+		end	
+	end
+end
+	--Check if this card was destroyed
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_BATTLE+REASON_EFFECT)
+end
+	--Check for opponent's monsters with A-Counters
+function s.accon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return c:GetCounter(0x1041)>0
+end
+	--Check for non-link reptile monsters that can be special summoned
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_REPTILE) and not c:IsType(TYPE_LINK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+	--Activation legality
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+			Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		end
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+	--Special summon reptile monsters from the GY
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ct=Duel.GetFieldGroupCount(s.accon,tp,LOCATION_MZONE)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	if ft<1 or ct<1 or g:GetCount()==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=g:Select(tp,1,1,nil)
+	g:Remove(Card.IsCode,nil,sg:GetFirst():GetCode())
+	if not Duel.IsPlayerAffectedByEffect(tp,59822133) then
+		ft=ft-1
+		ct=ct-1
+		while ft>0 and ct>0 and g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(36668118,2)) do
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg2=g:Select(tp,1,1,nil)
+			sg:AddCard(sg2:GetFirst())
+			g:Remove(Card.IsCode,nil,sg2:GetFirst():GetCode())
+			ft=ft-1
+			ct=ct-1
+		end
+	end
+	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+end
