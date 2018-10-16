b873fef561e2a55519b455077294a31bb85a8515
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-10-16 07:29:48 -0300

    Fixed filter name

--- Link Vrains Pack 2/c100235026.lua
+++ Link Vrains Pack 2/c100235026.lua
@@ -80,7 +80,7 @@ end
 	--Activation legality
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c22873798.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 	--Special summon reptile monsters from the GY
