cf4ddf72a4a9c4ad09596a01c9b104aab1b18bee
dest <dest.dn@gmail.com>
2018-11-15 14:12:47 +0100

    fix Utonomatopia
    function should not end when the card is not on the field upon resolution
    function should not end before the special summon restriction is applied
    added missing checks and procedure of g4
    special summon should not ignore summon conditions

--- 20TH-JP/c100236020.lua
+++ 20TH-JP/c100236020.lua
@@ -56,33 +56,38 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 	--Performing the effect of special summoning "Gagaga", "Gogogo", "Dododo", and/or "Zubaba" monster(s) from hand
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft==0 then return end
-	if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
 	local g1=Duel.GetMatchingGroup(s.spfilter1,tp,LOCATION_HAND,nil,e,tp)
 	local g2=Duel.GetMatchingGroup(s.spfilter2,tp,LOCATION_HAND,nil,e,tp)
 	local g3=Duel.GetMatchingGroup(s.spfilter3,tp,LOCATION_HAND,nil,e,tp)
 	local g4=Duel.GetMatchingGroup(s.spfilter4,tp,LOCATION_HAND,nil,e,tp)
-	local sg=Group.CreateGroup()
-	if g1:GetCount()>0 and ((g2:GetCount()==0 and g3:GetCount()==0) or Duel.SelectYesNo(tp,aux.Stringid(id,1))) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg1=g1:Select(tp,1,1,nil)
-		sg:Merge(sg1)
-		ft=ft-1
-	end
-	if g2:GetCount()>0 and ft>0 and ((sg:GetCount()==0 and g3:GetCount()==0) or Duel.SelectYesNo(tp,aux.Stringid(id,2))) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg2=g2:Select(tp,1,1,nil)
-		sg:Merge(sg2)
-		ft=ft-1
-	end
-	if g3:GetCount()>0 and ft>0 and (sg:GetCount()==0 or Duel.SelectYesNo(tp,aux.Stringid(id,3))) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg3=g3:Select(tp,1,1,nil)
-		sg:Merge(sg3)
-	end
-	Duel.SpecialSummon(sg,0,tp,tp,true,false,POS_FACEUP)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft>0 and (#g1>0 or #g2>0 or #g3>0 or #g4>0) then
+		if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
+		local sg=Group.CreateGroup()
+		if g1:GetCount()>0 and ((g2:GetCount()==0 and g3:GetCount()==0 and g4:GetCount()==0) or Duel.SelectYesNo(tp,aux.Stringid(id,1))) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg1=g1:Select(tp,1,1,nil)
+			sg:Merge(sg1)
+			ft=ft-1
+		end
+		if g2:GetCount()>0 and ft>0 and ((sg:GetCount()==0 and g3:GetCount()==0 and g4:GetCount()==0) or Duel.SelectYesNo(tp,aux.Stringid(id,2))) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg2=g2:Select(tp,1,1,nil)
+			sg:Merge(sg2)
+			ft=ft-1
+		end
+		if g3:GetCount()>0 and ft>0 and ((sg:GetCount()==0 and g4:GetCount()==0) or Duel.SelectYesNo(tp,aux.Stringid(id,3))) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg3=g3:Select(tp,1,1,nil)
+			sg:Merge(sg3)
+		end
+		if g4:GetCount()>0 and ft>0 and (sg:GetCount()==0 or Duel.SelectYesNo(tp,aux.Stringid(id,4)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg4=g4:Select(tp,1,1,nil)
+			sg:Merge(sg4)
+		end
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
 	end
 	local ge1=Effect.CreateEffect(c)
 	ge1:SetType(EFFECT_TYPE_FIELD)
