5ff255cca6c6da59c5bc0a1a55a05ea3eaaae459
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-11-15 06:25:45 -0200

    Removed LVP2 cards already online

--- Link Vrains Pack 2/Bushido/utility.lua
+++
@@ -1,7 +0,0 @@
-function aux.IsCounterAdded(c,counter)
-	if not c.counter_add_list then return false end
-		for _,ccounter in ipairs(c.counter_add_list) do
-			if counter==ccounter then return true end
-		end
-	return false
-end
