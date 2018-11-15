4f0c73e31c8a3ca47ba2c67f444ede0a8543dd67
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-11-15 06:21:41 -0200

    Removed cards already online

--- DP21-JP/proc_workaround.lua
+++
@@ -1,9 +0,0 @@
---Checks whether the card is located at any of the sequences passed as arguments.
-function Card.IsSequence(c,...)
-	local arg={...}
-	local seq=c:GetSequence()
-	for _,v in ipairs(arg) do
-		if seq==v then return true end
-	end
-	return false
-end
