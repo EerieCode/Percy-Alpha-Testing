f80350003d40b56f22a2aed5ec16fdf36893a870
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2018-11-17 05:36:05 -0500

    Minor fix
    Missed ID substitution.

--- Link Vrains Pack 2/c100235061.lua
+++ Link Vrains Pack 2/c100235061.lua
@@ -7,7 +7,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Link summon method
 	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,c63288573.matfilter,1,1)
+	aux.AddLinkProcedure(c,s.matfilter,1,1)
 	--Unaffected by trap effects, continuous effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
