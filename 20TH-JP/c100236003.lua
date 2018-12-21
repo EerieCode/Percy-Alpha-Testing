a2dd782b1a6f0fe6c63d36fe64b5a18c5bcda5cc
Naim <26061438+NaimSantos@users.noreply.github.com>
2018-12-21 09:18:54 -0200

    Fixed conditions for "Destiny Draw" and added temporary operation

--- 20TH-JP/c100236003.lua
+++ 20TH-JP/c100236003.lua
@@ -15,15 +15,18 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.drcon(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE)
-	local mg=g and Duel.GetMaxGroup(g,Card.GetAttack)
-	--current assumption on ruling is like Dinomist Brachion, "(even if it's tied)". 
-	--change to not mg:IsExists(...,tp) if rulings contradict
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	local mg=g and g:GetMaxGroup(Card.GetAttack)
 	return Duel.GetLP(tp)<Duel.GetLP(1-tp) and mg and mg:IsExists(Card.IsControler,1,nil,1-tp)
 end
+
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>2 end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
 end
