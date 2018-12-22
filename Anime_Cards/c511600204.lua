edb0bc6474e110557220f84e9f374e3556794eb7
Larry126 <larry126@users.noreply.github.com>
2018-12-22 11:55:53 +0800

    Add files via upload

--- Anime_Cards/c511600204.lua
+++ Anime_Cards/c511600204.lua
@@ -21,22 +21,18 @@ function s.initial_effect(c)
 	--atk down
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(alias,1))
-	e2:SetCategory(CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetRange(LOCATION_REMOVED)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_REMOVE)
 	e2:SetCondition(s.atkcon)
-	e2:SetTarget(s.atktg)
 	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
-	if not ClockLizardSubstitute then
-		ClockLizardSubstitute = {}
+	--workaround
+	if not ClockLizardSubstituteGroup then
 		ClockLizardSubstituteGroup = Group.CreateGroup()
 		ClockLizardSubstituteGroup:KeepAlive()
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
-		ge1:SetCountLimit(1)
 		ge1:SetCode(EVENT_ADJUST)
 		ge1:SetOperation(s.subop)
 		Duel.RegisterEffect(ge1,0)
@@ -79,12 +75,12 @@ function s.initial_effect(c)
 	local getmatchgc=Duel.GetMatchingGroupCount
 		Duel.GetMatchingGroupCount=function(f,tp,int_s,int_o,ex,...)
 		local arg={...}
-		return #Duel.GetMatchingGroup(f,tp,int_s,int_o,ex,arg)
+		return #Duel.GetMatchingGroup(f,tp,int_s,int_o,ex,table.unpack(arg))
 	end
 	local getfmatch=Duel.GetFirstMatchingCard
 		Duel.GetFirstMatchingCard=function(f,tp,int_s,int_o,ex,...)
 		local arg={...}
-		return Duel.GetMatchingGroup(f,tp,int_s,int_o,ex,arg):GetFirst()
+		return Duel.GetMatchingGroup(f,tp,int_s,int_o,ex,table.unpack(arg)):GetFirst()
 	end
 	local selmatchc=Duel.SelectMatchingCard
 		Duel.SelectMatchingCard=function(sp,f,tp,int_s,int_o,min,max,ex, ...)
@@ -98,7 +94,7 @@ function s.initial_effect(c)
 	local gettgc=Duel.GetTargetCount
 		Duel.GetTargetCount=function(f,tp,int_s,int_o,ex,...)
 		local arg={...}
-		return #Duel.GetTarget(f,tp,int_s,int_o,ex and ClockLizardSubstituteGroup+ex or ClockLizardSubstituteGroup,arg)
+		return gettgc(f,tp,int_s,int_o,ex and ClockLizardSubstituteGroup+ex or ClockLizardSubstituteGroup,table.unpack(arg))
 	end
 	local seltg=Duel.SelectTarget
 		Duel.SelectTarget=function(sp,f,tp,int_s,int_o,min,max,ex, ...)
@@ -115,11 +111,14 @@ function s.initial_effect(c)
 	end
 end
 function s.subop(e,tp,eg,ep,ev,re,r,rp,chk)
-	for i=0,1 do
-		local sub=Duel.CreateToken(i,511610204)
-		ClockLizardSubstitute[i] = sub
-		ClockLizardSubstituteGroup:AddCard(sub)
-		Duel.SendtoDeck(sub,nil,1,REASON_RULE)
+	for p=0,1 do
+		if Duel.IsExistingMatchingCard(Card.IsCode,p,0xff,0,1,nil,alias)
+			and Duel.GetFieldGroupCount(p,LOCATION_EXTRA,0)==0
+			and not ClockLizardSubstituteGroup:IsExists(Card.IsControler,1,nil,p) then
+			local sub=Duel.CreateToken(p,alias)
+			ClockLizardSubstituteGroup:AddCard(sub)
+			Duel.Sendto(sub,LOCATION_EXTRA,REASON_RULE)
+		end
 	end
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -129,12 +128,18 @@ end
 function s.mfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsCanBeFusionMaterial() and c:IsAbleToRemove() and aux.SpElimFilter(c)
 end
-function s.tdfilter(c,e,tp,mg,f,mgc,mf)
+function s.tdfilter(c,e,tp,mg,mgc,mf)
 	if not c:IsType(TYPE_FUSION) or not c:IsAbleToExtra()
-		or not (c:CheckFusionMaterial(mg,nil,tp) and (not f or f(c))
-		or c:CheckFusionMaterial(mgc,nil,tp) and (not mf or mf(c))) then return false end
-	local fc=ClockLizardSubstitute[tp]
+		or not (c:CheckFusionMaterial(mg,nil)
+		or c:CheckFusionMaterial(mgc,nil) and (not mf or mf(c))) then return false end
+	local fc=Duel.GetFieldGroupCount(tp,LOCATION_EXTRA,0)>0 and Duel.GetFirstMatchingCard(Card.IsFacedown,tp,LOCATION_EXTRA,0,nil)
+		or ClockLizardSubstituteGroup:Filter(Card.IsControler,nil,tp):GetFirst()
 	if Duel.GetLocationCountFromEx(tp,tp,e:GetHandler(),fc)<=0 then return false end
+	local fcspcon={}
+	for _,eff in ipairs({fc:GetCardEffect(EFFECT_SPSUMMON_CONDITION)}) do
+		table.insert(fcspcon,eff:Clone())
+		eff:Reset()
+	end
 	fc:AssumeProperty(ASSUME_CODE,c:GetOriginalCodeRule())
 	fc:AssumeProperty(ASSUME_TYPE,c:GetOriginalType())
 	fc:AssumeProperty(ASSUME_LEVEL,c:GetOriginalLevel())
@@ -145,17 +150,31 @@ function s.tdfilter(c,e,tp,mg,f,mgc,mf)
 	local fceffs={}
 	local sum=false
 	for _,eff in ipairs({c:GetCardEffect(EFFECT_SPSUMMON_CONDITION)}) do
-		local e=eff:Clone()
-		fc:RegisterEffect(e,true)
-		table.insert(fceffs,e)
+		local eClone=eff:Clone()
+		fc:RegisterEffect(eClone,true)
+		table.insert(fceffs,eClone)
 	end
 	if fc:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) then sum=true end
 	for _,eff in ipairs(fceffs) do
 		eff:Reset()
 	end
+	for _,eff in ipairs(fcspcon) do
+		fc:RegisterEffect(eff,true)
+	end
 	return sum
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		local mg=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE+LOCATION_ONFIELD,0,nil)
+		local ce=Duel.GetChainMaterial(tp)
+		local mgc=Group.CreateGroup()
+		local mf=nil
+		if ce~=nil then
+			mgc=ce:GetTarget()(ce,e,tp)
+			mf=ce:GetValue()
+		end
+		return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tdfilter(chkc,e,tp,mg,mgc,mf)
+	end
 	local mg=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE+LOCATION_ONFIELD,0,nil)
 	local ce=Duel.GetChainMaterial(tp)
 	local mgc=Group.CreateGroup()
@@ -164,10 +183,9 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		mgc=ce:GetTarget()(ce,e,tp)
 		mf=ce:GetValue()
 	end
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tdfilter(chkc,e,tp,mg,nil,mgc,mf) end
-	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,mg,nil,mgc,mf) end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,mg,mgc,mf) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,mg,nil,mgc,mf)
+	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,mg,mgc,mf)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,tp,LOCATION_GRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_FUSION_SUMMON,nil,1,tp,LOCATION_EXTRA)
@@ -182,7 +200,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		mgc=ce:GetTarget()(ce,e,tp)
 		mf=ce:GetValue()
 	end
-	if tc:IsRelateToEffect(e) and s.tdfilter(tc,e,tp,mg,nil,mgc,mf) and Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)~=0 then
+	if tc:IsRelateToEffect(e) and s.tdfilter(tc,e,tp,mg,mgc,mf) and Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)~=0 then
 		local nf=tc:CheckFusionMaterial(mg,nil,tp) and (not f or f(tc))
 		local cef=tc:CheckFusionMaterial(mgc,nil,tp) and (not mf or mf(tc))
 		if tc:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
@@ -203,29 +221,34 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function s.filter(c,p)
-	return c:IsControler(p) and c:IsFaceup()
-end
 function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetTurnID()==Duel.GetTurnCount()
-end
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return eg:IsExists(s.filter,1,nil,1-tp) end
-	local g=eg:Filter(s.filter,nil,1-tp)
-	Duel.SetTargetCard(g)
-	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,#g,1-tp,-Duel.GetMatchingGroupCount(Card.IsRace,e:GetOwnerPlayer(),LOCATION_GRAVE,0,nil,RACE_CYBERSE)*400)
+	return e:GetHandler():IsPreviousLocation(LOCATION_GRAVE)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
-	for tc in aux.Next(g) do
-		local e1=Effect.CreateEffect(e:GetHandler())
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTarget(s.con)
+	e1:SetOperation(s.op)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.filter(c,p)
+	return c:IsControler(p) and c:IsFaceup() and c:IsAttackAbove(1)
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp,chk)
+	return eg:IsExists(s.filter,1,nil,1-tp)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,tp,id)
+	Duel.Hint(HINT_CARD,1-tp,id)
+	local atk=Duel.GetMatchingGroupCount(Card.IsRace,tp,LOCATION_GRAVE,0,nil,RACE_CYBERSE)*400
+	for tc in aux.Next(eg:Filter(s.filter,nil,1-tp)) do
+		local e1=Effect.CreateEffect(e:GetOwner())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(s.val)
+		e1:SetValue(-atk)
 		e1:SetReset(RESET_PHASE+PHASE_END+RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
-end
-function s.val(e,c)
-	return Duel.GetMatchingGroupCount(Card.IsRace,e:GetOwnerPlayer(),LOCATION_GRAVE,0,nil,RACE_CYBERSE)*-400
 end
