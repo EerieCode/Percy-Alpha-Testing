198a67015a992ef8dab80bdde558d2bde72b5602
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-01-11 08:23:05 -0200

    Fixed "Assault Sniper"

--- DANE-JP/script/c101008011.lua
+++ DANE-JP/script/c101008011.lua
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--reveal
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
@@ -25,7 +25,6 @@ function s.initial_effect(c)
 	e2:SetTarget(s.rvtg)
 	e2:SetOperation(s.rvop)
 	c:RegisterEffect(e2)
-
 end
 s.listed_names={CARD_ASSAULT_MODE}
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -33,7 +32,7 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(e:GetHandler(),REASON_COST)
 end 
 function s.spfilter(c,e,tp)
-	return aux.IsCodeListed(CARD_ASSAULT_MODE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return aux.IsCodeListed(c,CARD_ASSAULT_MODE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
@@ -42,6 +41,7 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND)
 end 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler(c)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,1,nil,e,tp)
