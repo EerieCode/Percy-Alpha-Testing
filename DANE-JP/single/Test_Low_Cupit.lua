ce7a830bcaa81c54c86c0ce195b9ed9c3cf03fc0
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-01-11 10:12:26 -0200

    Create Test_Low_Cupit.lua

---
+++ DANE-JP/single/Test_Low_Cupit.lua
@@ -0,0 +1,16 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Monster Zones
+Debug.AddCard(101008028,0,0,LOCATION_MZONE,1,1,true)
+Debug.AddCard(101008028,0,0,LOCATION_MZONE,2,1,true)
+Debug.AddCard(101008028,0,0,LOCATION_MZONE,3,1,true)
+Debug.AddCard(101008028,0,0,LOCATION_MZONE,4,1,true)
+--Monster Zones
+Debug.AddCard(21417692,1,1,LOCATION_MZONE,3,1,true)
+Debug.AddCard(83629030,1,1,LOCATION_MZONE,2,1,true)
+Debug.AddCard(42589641,1,1,LOCATION_MZONE,1,1,true)
+Debug.AddCard(1861629,1,1,LOCATION_MZONE,4,1,true)
+Debug.ReloadFieldEnd()
