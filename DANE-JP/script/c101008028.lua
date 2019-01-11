082a487c54c007588a566e5ab62249c66d7635ec
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-01-11 10:19:22 -0200

    A CARD EXTREMELY ANNOYING

--- DANE-JP/script/c101008028.lua
+++ DANE-JP/script/c101008028.lua
@@ -7,6 +7,8 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	--e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	--e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(s.indval)
 	c:RegisterEffect(e1)
 	--to hand
@@ -20,7 +22,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function s.indval(e,c)
-	return c:GetLevel()>e:GetHandler():GetLevel()
+	return c:IsLevelBelow(e:GetHandler():GetLevel()) or c:IsType(TYPE_LINK) or c:IsType(TYPE_XYZ)
 end
 function s.lvcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
