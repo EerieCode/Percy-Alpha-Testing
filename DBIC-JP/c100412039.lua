3eb3f4538a439c987218b1fc0cf788c3e9c72bda
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-01-16 18:00:55 -0200

    Update c100412039.lua

--- DBIC-JP/c100412039.lua
+++ DBIC-JP/c100412039.lua
@@ -9,7 +9,6 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
-	
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
@@ -19,13 +18,13 @@ function s.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x226)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return rp~=tp and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
 		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_SZONE,0,1,CARD_CURSED_EYE_SELENE) then
+	if Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsCode,CARD_CURSED_EYE_SELENE),tp,LOCATION_SZONE,0,1,nil) then
 		e:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
 	end
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
