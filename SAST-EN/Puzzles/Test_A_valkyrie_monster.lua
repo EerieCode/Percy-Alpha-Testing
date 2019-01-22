d09e73bdaead344c41b21ecf1b436b640d2d4348
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-01-22 16:39:20 -0200

    Adding sub folders

---
+++ SAST-EN/Puzzles/Test_A_valkyrie_monster.lua
@@ -0,0 +1,21 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Main Deck
+Debug.AddCard(73594093,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(2204038,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Hand
+Debug.AddCard(35726888,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(81439173,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(19230407,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(71564252,0,0,LOCATION_GRAVE,0,POS_FACEDOWN)
+--Monster Zones
+Debug.AddCard(101007089,0,0,LOCATION_MZONE,1,1,true)
+Debug.AddCard(59919307,0,0,LOCATION_SZONE,1,10)
+Debug.AddCard(66809920,0,0,LOCATION_MZONE,2,1,true)
+Debug.AddCard(66809920,0,0,LOCATION_MZONE,3,1,true)
+Debug.AddCard(20721928,1,1,LOCATION_MZONE,4,1,true)
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
