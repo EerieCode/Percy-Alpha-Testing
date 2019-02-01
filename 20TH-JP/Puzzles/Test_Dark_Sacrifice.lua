eff6a0798bb518ab7a4812efe160ca96db85c82a
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-02-01 10:32:29 -0200

    Added "Dark Sacrifice"

---
+++ 20TH-JP/Puzzles/Test_Dark_Sacrifice.lua
@@ -0,0 +1,21 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Spell & Trap Zones
+Debug.AddCard(100236113,0,0,LOCATION_SZONE,1,10)
+--Hand
+Debug.AddCard(55885348,1,1,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(31061682,1,1,LOCATION_HAND,0,POS_FACEDOWN)
+
+Debug.AddCard(55885348,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(31061682,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+
+Debug.AddCard(26202165,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Monster Zones
+Debug.AddCard( 44095762,1,1,LOCATION_SZONE,3,10)
+Debug.AddCard(75878039,1,1,LOCATION_MZONE,3,1,true)
+Debug.AddCard(75878039,0,0,LOCATION_MZONE,3,1,true)
+Debug.AddCard( 44095762,0,0,LOCATION_SZONE,3,10)
+Debug.ReloadFieldEnd()
