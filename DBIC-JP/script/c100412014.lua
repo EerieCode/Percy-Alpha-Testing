cb860515b2926d358cdda1d388bd40bca1ffbd69
AlphaKretin <deewhydeeecks@gmail.com>
2019-02-12 15:59:15 +1030

    Fix function name

--- DBIC-JP/script/c100412014.lua
+++ DBIC-JP/script/c100412014.lua
@@ -72,7 +72,7 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
 end
-function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
     if tc:IsRelateToEffect(e) then
         Duel.SendtoHand(tc,nil,REASON_EFFECT)
