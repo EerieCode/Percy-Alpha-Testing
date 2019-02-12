5109ec863e5e13712d4ef4f2bf72eb6a7804549a
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-02-12 10:30:12 -0200

    Update WC Discard.lua

--- DBIC-JP/WC Discard.lua
+++ DBIC-JP/WC Discard.lua
@@ -3,7 +3,7 @@ function Auxiliary.WitchcraftDiscardFilter(c)
 end
 function Auxiliary.WitchcraftDiscardGroup(minc)
 	return	function(sg,e,tp,mg)
-				if sg:IsExists(Card.IsHasEffect,100412024) then
+				if sg:IsExists(Card.IsHasEffect,1,nil,100412024) then
 					return #sg==1
 				else
 					return #sg>=minc
@@ -19,7 +19,7 @@ function Auxiliary.WitchcraftDiscardCost(f,minc,maxc)
 				local g=Duel.GetMatchingGroup(f,tp,LOCATION_HAND,0,nil)
 				g:Merge(Duel.GetMatchingGroup(Auxiliary.WitchcraftDiscardFilter,tp,LOCATION_ONFIELD,0,nil))
 				local sg=Auxiliary.SelectUnselectGroup(g,e,tp,1,maxc,Auxiliary.WitchcraftDiscardGroup(minc),1,tp,HINTMSG_DISCARD)
-				if sg:IsExists(Card.IsHasEffect,100412024) then
+				if sg:IsExists(Card.IsHasEffect,1,nil,100412024) then
 					local id=sg:GetFirst():GetOriginalCode()
 					Duel.SendtoGrave(sg,REASON_COST)
 					Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
