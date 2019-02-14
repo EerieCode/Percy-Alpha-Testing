4a8ab3dd1ee51b4d428724a609644963e07b036c
AlphaKretin <deewhydeeecks@gmail.com>
2019-02-14 17:08:55 +1030

    Fix "Guidance of Spell Salvation"
    Must return an Effect monster

--- SR08-JP/script/c100308041.lua
+++ SR08-JP/script/c100308041.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
     c:RegisterEffect(e1)
 end
 function s.filter(c)
-    return c:IsRace(RACE_SPELLCASTER) and c:IsAbleToHand()
+    return c:IsRace(RACE_SPELLCASTER) and c:IsType(TYPE_EFFECT) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:GetControler()==tp and chkc:GetLocation()==LOCATION_GRAVE and s.filter(chkc) end
