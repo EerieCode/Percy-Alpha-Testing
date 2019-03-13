36a1b36dbc2c60509d9ffc1dc920bf02ac1ad9ec
AlphaKretin <deewhydeeecks@gmail.com>
2019-03-13 21:34:12 +1030

    Fix "Draw Discharge"

--- RIRA-JP/c101009067.lua
+++ RIRA-JP/c101009067.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and r&REASON_EFFECT~=0
+	return ep~=tp and r&REASON_EFFECT~=0 and rp==ep
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
