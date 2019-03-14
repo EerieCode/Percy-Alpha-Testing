e3a3ae93bdfc377c60f485f011481a82c4bc03c8
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-03-14 15:42:58 -0300

    Create Test_A_Continuous_Fortune_Lady_Card.lua

---
+++ RIRA-JP/Puzzles/Test_A_Continuous_Fortune_Lady_Card.lua
@@ -0,0 +1,27 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Main Deck
+Debug.AddCard(34471458,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(29088922,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Hand
+Debug.AddCard(101009051 ,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(29491334,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(89882100,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(70231910,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(70231910,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(5318639,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--GY
+Debug.AddCard(71870152,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+--Monster Zones
+Debug.AddCard(31061682,0,0,LOCATION_MZONE,1,1,true)
+--Spell & Trap Zones
+Debug.AddCard(36261276,0,0,LOCATION_SZONE,1,10)
+--Monster Zones
+Debug.AddCard(69327790,1,1,LOCATION_MZONE,3,1,true)
+--Spell & Trap Zones
+Debug.AddCard(44095762,1,1,LOCATION_SZONE,2,10)
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
