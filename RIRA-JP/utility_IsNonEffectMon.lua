f64875d507b595f3882d9a43bf7d0e152bd5f8bb
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-03-15 22:28:11 +0200

    Update utility_IsNonEffectMon.lua

--- RIRA-JP/utility_IsNonEffectMon.lua
+++ RIRA-JP/utility_IsNonEffectMon.lua
@@ -1,4 +1,4 @@
 --utility function for a "non-effect monster"
 function Card.IsNonEffectMon(c)
-    return c:GetType()==TYPE_MONSTER and not c:GetType()==TYPE_EFFECT
+    return c:IsType(TYPE_MONSTER) and not c:IsType(TYPE_EFFECT)
 end
