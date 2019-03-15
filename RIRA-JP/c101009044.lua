268c918a06ca911da7c073accbd9fcff5f284c4d
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-03-15 10:05:08 -0300

    Update c101009044.lua

--- RIRA-JP/c101009044.lua
+++ RIRA-JP/c101009044.lua
@@ -46,7 +46,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		local e1=Effect.CreateEffect(c)
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
