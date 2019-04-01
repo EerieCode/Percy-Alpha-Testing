d95df8e22d4d846c8031aca1f32a6445ab15be69
Naim <26061438+NaimSantos@users.noreply.github.com>
2019-04-01 12:39:23 -0300

    Finished "Mayosenju Hitotsumenomuraji"

--- RIRA-JP/c101009011.lua
+++ RIRA-JP/c101009011.lua
@@ -15,7 +15,7 @@ function c101009011.initial_effect(c)
 	e1:SetTarget(s.sctg)
 	e1:SetOperation(s.scop)
 	c:RegisterEffect(e1)
-	--back to to hand
+	--return itself to hand
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND)
@@ -23,9 +23,9 @@ function c101009011.initial_effect(c)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
 	e2:SetRange(LOCATION_PZONE)
 	e2:SetCountLimit(1,id+100)
-	e2:SetCondition(s.thcond)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
+	e2:SetCondition(s.sthcond)
+	e2:SetTarget(s.sthtg)
+	e2:SetOperation(s.sthop)
 	c:RegisterEffect(e2)
 	--spsummon limit
 	local e3=Effect.CreateEffect(c)
@@ -34,6 +34,55 @@ function c101009011.initial_effect(c)
 	e3:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e3:SetValue(aux.penlimit)
 	c:RegisterEffect(e3)
+	--reurn target to hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e5)
+	--update ATK
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetCategory(CATEGORY_ATKCHANGE)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_TO_HAND)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCondition(s.atkcond)
+	e6:SetOperation(s.atkop)
+	c:RegisterEffect(e6)
+	local e7=e6:Clone()
+	e7:SetCode(EVENT_TO_DECK)
+	e7:SetCondition(s.atkcond2)
+	c:RegisterEffect(e7)
+	--return itself to hand
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,2))
+	e8:SetCategory(CATEGORY_TOHAND)
+	e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetCountLimit(1)
+	e8:SetCode(EVENT_PHASE+PHASE_END)
+	e8:SetCondition(s.retcon)
+	e8:SetTarget(s.rettg)
+	e8:SetOperation(s.retop)
+	c:RegisterEffect(e8)
+	if not s.global_check then
+		s.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetLabel(id)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		ge1:SetOperation(aux.sumreg)
+		Duel.RegisterEffect(ge1,0)
+	end
 end
 function s.sctg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_PZONE) and chkc:IsControler(tp) and chkc:IsSetCard(0xb3) end
@@ -58,7 +107,7 @@ function s.scop(e,tp,eg,ep,ev,re,r,rp)
 		local e3=Effect.CreateEffect(c)
 		e3:SetType(EFFECT_TYPE_FIELD)
 		e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-		e3:SetDescription(aux.Stringid(id,1))
+		e3:SetDescription(aux.Stringid(id,4))
 		e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
 		e3:SetTargetRange(1,0)
 		e3:SetTarget(s.splimit)
@@ -69,15 +118,70 @@ end
 function s.splimit(e,c)
 	return not c:IsType(TYPE_SYNCHRO) and c:IsLocation(LOCATION_EXTRA)
 end
-function s.thcond(e,tp,eg,ep,ev,re,r,rp)
+function s.sthcond(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sthtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
+function s.sthop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SendtoHand(c,nil,REASON_EFFECT)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() and chkc:IsController(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
+function s.rtfilter(c,e,tp)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==tp
+end
+function s.atkcond(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.rtfilter,1,nil,e,tp)
+end
+function s.atkfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xb3)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+	end
+end
+function s.rtfilter2(c,e,tp)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==tp and not c:IsLocation(LOCATION_EXTRA)
+end
+function s.atkcond2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.rtfilter,1,nil,e,tp)
+end
+function s.retcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)~=0
+end
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+	end
 end
