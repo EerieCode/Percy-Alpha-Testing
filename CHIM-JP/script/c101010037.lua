a915e3d05de016f67808d5f3b8a535dd2fd3568d
EerieCode <eeriecode@gmail.com>
2019-05-15 19:57:46 +0200

    fix

--- CHIM-JP/script/c101010037.lua
+++ CHIM-JP/script/c101010037.lua
@@ -4,100 +4,82 @@
 local s,id=GetID()
 local COUNTER_FW=0x14c
 function s.initial_effect(c)
-	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),3)
-	--counter
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_COUNTER)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCondition(s.ctcon)
-	e1:SetTarget(s.cttg)
-	e1:SetOperation(s.ctop)
-	c:RegisterFlagEffect(e1)
-	--atk up
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.atkcon)
-	e2:SetValue(s.atkval)
-	c:RegisterEffect(e2)
-	--negate
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_NEGATE)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_CHAINING)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(s.discon)
-	e3:SetCost(s.discost)
-	e3:SetTarget(s.distg)
-	e3:SetOperation(s.disop)
-	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e4:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e4:SetOperation(s.flagop1)
-	c:RegisterEffect(e4)
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e5:SetCode(EVENT_DAMAGE_STEP_END)
-	e5:SetOperation(s.flagop2)
-	c:RegisterEffect(e5)
+    c:EnableReviveLimit()
+    aux.AddLinkProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),3)
+    --counter
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_COUNTER)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCondition(s.ctcon)
+    e1:SetTarget(s.cttg)
+    e1:SetOperation(s.ctop)
+    c:RegisterFlagEffect(e1)
+    --atk up
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCondition(s.atkcon)
+    e2:SetValue(s.atkval)
+    c:RegisterEffect(e2)
+    --negate
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_NEGATE)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_CHAINING)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCondition(s.discon)
+    e3:SetCost(s.discost)
+    e3:SetTarget(s.distg)
+    e3:SetOperation(s.disop)
+    c:RegisterEffect(e3)
 end
 function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
 function s.ctfilter(c)
-	return c:IsType(TYPE_FUSION+TYPE_RITUAL+TYPE_SYNCHRO+TYPE_XYZ) and c:IsRace(RACE_CYBERSE)
+    return c:IsType(TYPE_FUSION+TYPE_RITUAL+TYPE_SYNCHRO+TYPE_XYZ) and c:IsRace(RACE_CYBERSE)
 end
 function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.ctfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.ctfilter,tp,LOCATION_GRAVE,0,1,nil) end
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-	local g=Duel.GetMatchingGroup(s.ctfilter,tp,LOCATION_GRAVE,0,nil)
-	local ct=0
-	for _,ty in ipairs({TYPE_FUSION,TYPE_RITUAL,TYPE_SYNCHRO,TYPE_XYZ }) do
-		if g:IsExists(Card.IsType,1,nil,ty) then ct=ct+1 end
-	end
-	c:AddCounter(COUNTER_FW,ct)
+    local c=e:GetHandler()
+    if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+    local g=Duel.GetMatchingGroup(s.ctfilter,tp,LOCATION_GRAVE,0,nil)
+    local ct=0
+    for _,ty in ipairs({TYPE_FUSION,TYPE_RITUAL,TYPE_SYNCHRO,TYPE_XYZ }) do
+        if g:IsExists(Card.IsType,1,nil,ty) then ct=ct+1 end
+    end
+    c:AddCounter(COUNTER_FW,ct)
 end
 function s.atkcon(e)
-	local ph=Duel.GetCurrentPhase()
-	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+    local ph=Duel.GetCurrentPhase()
+    return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
 end
 function s.atkval(e,c)
-	return c:GetCounter(COUNTER_FW)*2500
+    return c:GetCounter(COUNTER_FW)*2500
 end
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
-	return rp~=tp and re:IsActiveType(TYPE_MONSTER) and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
+    return rp~=tp and re:IsActiveType(TYPE_MONSTER) and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and Duel.IsChainNegatable(ev)
 end
 function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,COUNTER_FW,1,REASON_COST) end
-	e:GetHandler():RemoveCounter(tp,COUNTER_FW,1,REASON_COST)
+    if chk==0 then return e:GetHandler():IsCanRemoveCounter(tp,COUNTER_FW,1,REASON_COST) end
+    e:GetHandler():RemoveCounter(tp,COUNTER_FW,1,REASON_COST)
 end
 function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.NegateActivation(ev) and c:IsFaceup() and c:IsRelateToEffect(e) 
-		and c:GetFlagEffect(id)~=0 and c:IsChainAttackable() then
-		Duel.ChainAttack()
-	end
-end
-function s.flagop1(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE,0,1)
-end
-function s.flagop2(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:GetFlagEffect(id)~=0 then c:ResetFlagEffect(id) end
+    local c=e:GetHandler()
+    if Duel.NegateActivation(ev) and c:IsFaceup() and c:IsRelateToEffect(e) 
+        and Duel.GetAttacker()==c and c:IsChainAttackable() then
+        Duel.ChainAttack()
+    end
 end
