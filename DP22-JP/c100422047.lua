4dfc50b39a45341808ebfca73607bc6f3f7e3a41
EerieCode <eeriecode@gmail.com>
2019-05-22 13:20:01 +0200

    fix

--- DP22-JP/c100422047.lua
+++ DP22-JP/c100422047.lua
@@ -16,7 +16,7 @@ function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x10f3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.thfilter(c)
-	return c:IsSetCard(0xf3) and c:IsAbleToHand()
+	return c:IsSetCard(0xf3) and not c:IsCode(id) and c:IsAbleToHand()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
