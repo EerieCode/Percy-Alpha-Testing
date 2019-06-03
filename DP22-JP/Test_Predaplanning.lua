1ef93bcae9362ea23b42de5de2d949e2cf6cf70d
Naim <naimsantos45@gmail.com>
2019-06-03 14:58:25 -0300

    Create Test_Predaplanning.lua

---
+++ DP22-JP/Test_Predaplanning.lua
@@ -0,0 +1,31 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Main Deck
+Debug.AddCard(89181134,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Extra Deck
+Debug.AddCard(41209827,0,0,LOCATION_EXTRA,0,8)
+Debug.AddCard(75286621,0,0,LOCATION_EXTRA,0,8)
+--Hand
+Debug.AddCard(24094653,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(24094653,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(24094653,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(38517737,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(35272499,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--GY
+Debug.AddCard(100422049,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+--Monster Zones
+Debug.AddCard(32448765,0,0,LOCATION_MZONE,5,1,true)
+Debug.AddCard(86120751,0,0,LOCATION_MZONE,1,4,true)
+Debug.AddCard(86120751,0,0,LOCATION_MZONE,3,4,true)
+Debug.AddCard(86120751,0,0,LOCATION_MZONE,2,4,true)
+Debug.AddCard(86120751,0,0,LOCATION_MZONE,4,4,true)
+--Spell & Trap Zones
+Debug.AddCard(59160188,0,0,LOCATION_SZONE,5,10)
+Debug.AddCard(100422049,0,0,LOCATION_SZONE,3,10)
+--Monster Zones
+Debug.AddCard(75878039,1,1,LOCATION_MZONE,3,1,true)
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
