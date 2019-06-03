6ae34fa1db0558027d45479034f663bb66579ce0
Naim <naimsantos45@gmail.com>
2019-06-03 15:32:13 -0300

    Create Test_EarthboundTrap.lua

---
+++ DP22-JP/Test_EarthboundTrap.lua
@@ -0,0 +1,33 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Hand
+Debug.AddCard(41181774,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(83764718,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(83764718,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(83764718,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(83764718,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--GY
+Debug.AddCard(79798060,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+--Monster Zones
+Debug.AddCard(75878039,0,0,LOCATION_MZONE,1,1,true)
+Debug.AddCard(75878039,0,0,LOCATION_MZONE,2,1,true)
+Debug.AddCard(75878039,0,0,LOCATION_MZONE,3,1,true)
+Debug.AddCard(75878039,0,0,LOCATION_MZONE,4,1,true)
+
+--Monster Zones
+Debug.AddCard(75878039,1,1,LOCATION_MZONE,1,1,true)
+Debug.AddCard(75878039,1,1,LOCATION_MZONE,2,1,true)
+Debug.AddCard(75878039,1,1,LOCATION_MZONE,3,1,true)
+Debug.AddCard(75878039,1,1,LOCATION_MZONE,4,1,true)
+
+--Spell & Trap Zones
+Debug.AddCard(100422027,0,0,LOCATION_SZONE,1,10)
+
+Debug.AddCard(18161786,0,0,LOCATION_SZONE,5,5)
+
+Debug.AddCard(18161786,1,1,LOCATION_SZONE,5,5)
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
