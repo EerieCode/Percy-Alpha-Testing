ca15f235b2c0c78e3971688ee393eae3d49ad44a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-06-03 19:29:03 +0300

    "Supreme King Castle" small fix

--- DP22-JP/c100422015.lua
+++ DP22-JP/c100422015.lua
@@ -50,7 +50,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	local ct=e:GetLabel()*200
 	if tc:IsRelateToBattle() and tc:IsFaceup() and tc:IsControler(tp) then
-		local e1=Effect.CreateEffect(c)
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(ct)
