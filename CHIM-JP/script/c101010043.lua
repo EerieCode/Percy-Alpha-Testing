3786575f8bf4719a25b8cbeb606b4d05fe66bcc6
Naim <naimsantos45@gmail.com>
2019-06-15 11:10:09 -0300

    Added placeholder scripts

---
+++ CHIM-JP/script/c101010043.lua
@@ -0,0 +1,7 @@
+--破は械かい神しんラギア
+--Hakaishin Ragia
+--Scripted by
+local s,id=GetID()
+function s.initial_effect(c)
+
+end
---
+++ CHIM-JP/script/c101010044.lua
@@ -0,0 +1,7 @@
+--
+--Hakaishin Arba
+--Scripted by
+local s,id=GetID()
+function s.initial_effect(c)
+
+end
