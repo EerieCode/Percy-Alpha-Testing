e6d9e81ae178fd91e1bb20eeb7b12b3a2942b28a
AlphaKretin <deewhydeeecks@gmail.com>
2019-06-17 21:24:00 +0930

    Add "Cluster Zanki Laplacian"

--- DBMF-JP/c100423009.lua
+++ DBMF-JP/c100423009.lua
@@ -9,6 +9,7 @@ function s.initial_effect(c)
     --atk
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCondition(s.con)
@@ -16,7 +17,16 @@ function s.initial_effect(c)
 	e1:SetTarget(s.tg)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+	--destroy replace
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	c:RegisterEffect(e2)
 end
+s.listed_series={0x231}
 function s.con(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
 end
@@ -28,11 +38,49 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if s.sttg(e,tp,eg,ep,ev,re,r,rp,0) then ct=ct+1 end
 	if chk==0 then return ct>0 and c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	c:RemoveOverlayCard(tp,1,ct,REASON_COST)
-	local num=Duel.GetOperatedGroup():GetCount()
-	e:SetLabel(num)
+	local ft=Duel.GetOperatedGroup():GetCount()
+	e:SetLabel(ft)
 end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	
+	local hand=s.handtg(e,tp,eg,ep,ev,re,r,rp,0)
+	local mon=s.montg(e,tp,eg,ep,ev,re,r,rp,0)
+	local st=s.sttg(e,tp,eg,ep,ev,re,r,rp,0)
+	if chk==0 then return hand or mon or st end
+	local ct=0
+	if hand then ct=ct+1 end
+	if mon then ct=ct+1 end
+	if st then ct=ct+1 end
+	local ft=e:GetLabel()
+	if ft<ct then ct=ft end
+	local sel
+	while ft>0 do
+		if hand and sel&0x1=0 and Duel.SelectYesNo(aux.Stringid(id,1)) then
+			sel=sel+0x1
+			Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,1))
+		end
+		if mon and sel&0x2=0 and Duel.SelectYesNo(aux.Stringid(id,2)) then
+			sel=sel+0x2
+			Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,2))
+		end
+		if st and sel&0x4=0 and Duel.SelectYesNo(aux.Stringid(id,3)) then
+			sel=sel+0x4
+			Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,3))
+		end
+		ft=ft-1
+	end
+	e:SetLabel(sel)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local sel=e:GetLabel()
+	if sel&0x1==0x1 then
+		s.handop(e,tp,eg,ep,ev,re,r,rp)
+	end
+	if sel&0x2==0x2 then
+		s.monop(e,tp,eg,ep,ev,re,r,rp)
+	end
+	if sel&0x4==0x4 then
+		s.setop(e,tp,eg,ep,ev,re,r,rp)
+	end
 end
 function s.handtg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)~=0 end
@@ -46,7 +94,6 @@ function s.handop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.montg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,0,LOCATION_MZONE,1,nil) end
-	e:SetCategory(CATEGORY_TOGRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,0)
 end
 function s.monop(e,tp,eg,ep,ev,re,r,rp)
@@ -60,7 +107,6 @@ function s.stfilter(c)
 end
 function s.sttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.stfilter,tp,0,LOCATION_MZONE,1,nil) end
-	e:SetCategory(CATEGORY_TOGRAVE)
 	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,0)
 end
 function s.monop(e,tp,eg,ep,ev,re,r,rp)
@@ -68,4 +114,20 @@ function s.monop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
 	end
+end
+function s.repfilter(c,tp)
+	return c:IsSetCard(0x231) and c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
+		and c:IsReason(REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local g=c:GetLinkedGroup()
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) and c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
+		return true
+	else return false end
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
 end
