b8aaba31ca4ddb7ee1e09dfc1aa3f93b96bd1956
Naim <naimsantos45@gmail.com>
2019-06-19 15:59:44 -0300

    Update Test Reiji.lua

--- YA07-JP/Test Reiji.lua
+++ YA07-JP/Test Reiji.lua
@@ -1,7 +1,7 @@
 --Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
 Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
-Debug.SetPlayerInfo(0,3950,0,0)
-Debug.SetPlayerInfo(1,8000,0,0)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,3955,0,0)
 
 --Hand
 Debug.AddCard(46918794,0,0,LOCATION_HAND,0,POS_FACEDOWN)
