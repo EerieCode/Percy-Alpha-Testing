db8a304c04b6c2d181c3f2291382c6b585b58179
Naim <naimsantos45@gmail.com>
2019-07-12 10:36:58 -0300

    Update c101010079.lua

--- CHIM-JP/script/c101010079.lua
+++ CHIM-JP/script/c101010079.lua
@@ -8,15 +8,21 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Only control 1
 	c:SetUniqueOnField(1,0,id)
-	--Send 3 cards from opponent's deck to GY
+	--activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DECKDES)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(s.condition)
-	e1:SetOperation(s.operation)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_SPSUMMON)
 	c:RegisterEffect(e1)
+	--Send 3 cards from opponent's deck to GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DECKDES)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.condition)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
 end
 	--Who summoned
 function s.filter(c,tp)
