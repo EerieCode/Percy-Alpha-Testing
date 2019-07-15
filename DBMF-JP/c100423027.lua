5b8a47c4209a3a1c6f7db784c7c0526bc292d1a0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-07-15 22:25:21 +0300

    Update c100423027.lua

--- DBMF-JP/c100423027.lua
+++ DBMF-JP/c100423027.lua
@@ -19,16 +19,16 @@ function s.initial_effect(c)
     e2:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e2)
 end
-function s.thfilter(c,code)
-    return not c:IsCode(code) and (c:IsRace(RACE_PLANT) or c:IsSetCard(0x232)) and c:IsAbleToHand()
+function s.thfilter(c)
+    return not c:IsCode(id) and (c:IsRace(RACE_PLANT) or c:IsSetCard(0x232)) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e:GetHandler():GetCode()) end
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e:GetHandler():GetCode())
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
     if #g>0 then
         Duel.SendtoHand(g,nil,REASON_EFFECT)
         Duel.ConfirmCards(1-tp,g)
