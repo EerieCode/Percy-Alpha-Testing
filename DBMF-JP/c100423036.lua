ceb6c89a7a4864f5b087e90b0bea24aea9b37295
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-07-15 23:59:42 +0300

    reward small fix

--- DBMF-JP/c100423036.lua
+++ DBMF-JP/c100423036.lua
@@ -47,7 +47,7 @@ function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(1)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,1-tp,1)
 end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():IsRelateToEffect(e) then
