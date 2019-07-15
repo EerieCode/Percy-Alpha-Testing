12b1a4231860a6a95c5e5b70b7c40528e736c86a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-07-15 22:29:44 +0300

    Quest update

--- DBMF-JP/c100423035.lua
+++ DBMF-JP/c100423035.lua
@@ -5,8 +5,9 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--search
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
@@ -16,7 +17,7 @@ function s.cfilter(c)
 	return c:IsSetCard(0x232) and c:IsType(TYPE_MONSTER) and not c:IsPublic() and c:IsAbleToDeck()
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x232) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+	return not c:IsCode(id) and c:IsSetCard(0x232) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
 end
 function s.rescon(sg,e,tp,mg)
     return sg:GetClassCount(Card.GetCode)==#sg
@@ -24,8 +25,8 @@ end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) 
-		and aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,chk)
-	cDuel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+		and aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,0) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
@@ -36,6 +37,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,rc)
 		local sg=aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,1,tp,HINTMSG_ATOHAND)
 		if #sg>0 and Duel.SendtoHand(sg,nil,REASON_EFFECT)~=0 then
+			Duel.ShuffleDeck(tp)
 			Duel.BreakEffect()
 			Duel.SendtoDeck(rc,nil,1,REASON_EFFECT)
 		end
