3319af9ae5aa5e3cd61abbeca933406fc2c440a6
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2019-07-17 15:54:30 -0400

    Small fix
    Legit blind.

--- RIRA-EN/script/c101009086.lua
+++ RIRA-EN/script/c101009086.lua
@@ -67,7 +67,7 @@ end
 function s.dsptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
-	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.dspfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.dspfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.dspop(e,tp,eg,ep,ev,re,r,rp)
