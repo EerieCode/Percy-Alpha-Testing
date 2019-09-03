c3f625c08f500ba12edda90b26c8a2a49c177f4f
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2019-09-03 01:15:04 -0400

    Updated "Valkyrie Sigrún"
    Changed the category of the first effect.

--- V-Jump/c100200171.lua
+++ V-Jump/c100200171.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	--Special summon from hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
