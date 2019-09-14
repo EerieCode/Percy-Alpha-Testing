d50fa2e8eec0933ae53aa48927f4cfaa293e65eb
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2019-09-14 04:40:52 -0400

    Updated "Origin Dragon Primal Dragon"

--- V-Jump/c100200172.lua
+++ V-Jump/c100200172.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetCountLimit(1,id)
+	e2:SetCountLimit(1,id)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCost(s.cost)
 	e2:SetOperation(s.operation)
@@ -55,7 +55,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterEffect(e1)
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		tc:RegisterEffect(e2)
+		c:RegisterEffect(e2)
 	end
 end
 	--Activation legality
