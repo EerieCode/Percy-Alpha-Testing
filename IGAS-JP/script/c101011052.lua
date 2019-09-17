53b53c71021fcd1ddd20ce11d0675cf53a96ab0f
Larry126 <larry126@users.noreply.github.com>
2019-09-17 23:31:24 +0800

    Added 0x235 (A.I.) to listed_series

--- IGAS-JP/script/c101011052.lua
+++ IGAS-JP/script/c101011052.lua
@@ -28,7 +28,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x234}
+s.listed_series={0x234,0x235}
 function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x234) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
