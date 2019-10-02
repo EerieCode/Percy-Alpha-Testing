1e12a1c4cb65b75c31b4ca5cf0f41abe32c85db2
NaimSantos <naimsantos45@gmail.com>
2019-10-02 14:14:01 -0300

    Fixed "Senka Adonis - Zhou Gong"

--- IGAS-JP/script/c101011009.lua
+++ IGAS-JP/script/c101011009.lua
@@ -1,42 +1,37 @@
--- Shuu Kou the Beauty of Senka
--- Level 4 WATER Beast-Warrior Effect Monster
--- 1600 ATK / 1600 DEF
--- -You can only use each of this card name's (1) and (2) effects once per turn.
--- (1) You can target 1 Continuous Spell or Continuous Trap you control; send it to the GY, and if you do, add 1 "Senka" Spell/Trap from your Deck to your hand.<Ignition>
--- (2) If you activate the effect of another "Senka" monster: You can target 1 Effect Monster your opponent controls; negate its effects until the end of this turn.<Trigger>
+--戦華の美－周公
+--Senka Adonis - Zhou Gong
 local s,id=GetID()
 function s.initial_effect(c)
-	
 	--search
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
-	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
 	--negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(aux.chainreg)
+	c:RegisterEffect(e2)
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e3:SetCode(EVENT_CHAINING)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_CHAIN_SOLVED)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetOperation(aux.chainreg)
+	e3:SetCountLimit(1,id+1)
+	e3:SetCondition(s.negcon)
+	e3:SetTarget(s.negtg)
+	e3:SetOperation(s.negop)
 	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_TOHAND)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e4:SetCode(EVENT_CHAIN_SOLVED)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,id+1)
-	e4:SetCondition(s.negcon)
-	e4:SetTarget(s.negtg)
-	e4:SetOperation(s.negop)
-	c:RegisterEffect(e4)
 end
 s.listed_names={101011009}
 s.listed_series={0x236}
@@ -47,7 +42,7 @@ end
 function s.thfilter(c)
 	return c:IsSetCard(0x236) and c:IsAbleToHand() and c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
-function s.thtf(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(tp) and s.gvfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.gvfilter,tp,LOCATION_MZONE,0,1,nil)
 		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
@@ -69,7 +64,6 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 			Duel.ConfirmCards(1-tp,g)
 		end
 	end
-	
 end
 --negate
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
