bdcef85dce56578853c1510a48434a20f5bc4700
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2019-10-03 16:43:23 +0800

    description fix for Soukyoku no Hakaishin

--- IGAS-JP/script/c101011019.lua
+++ IGAS-JP/script/c101011019.lua
@@ -36,7 +36,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 	--revive
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(19337371,0))
+	e4:SetDescription(aux.Stringid(id,2))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e4:SetRange(LOCATION_GRAVE)
