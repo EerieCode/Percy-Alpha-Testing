898ea4717083ebd81cebdc2a7a37e48e377ca5ef
NaimSantos <naimsantos45@gmail.com>
2019-10-03 10:59:25 -0300

    Fixed "Megalith Promotion"

--- IGAS-JP/script/c101011071.lua
+++ IGAS-JP/script/c101011071.lua
@@ -37,22 +37,21 @@ end
 function s.filter(c)
 	return c:IsFaceup() and c:IsLevelAbove(1) and c:IsLevelBelow(4)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e1:SetCode(EFFECT_UPDATE_LEVEL)
 		e1:SetValue(tc:GetLevel()*2)
 		tc:RegisterEffect(e1)
 	end
