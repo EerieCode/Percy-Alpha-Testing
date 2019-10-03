1d64c83bc00a5b031dbf3a83ce7a508e781ce71e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-10-03 22:49:19 +0300

    Update c101010087.lua

--- CHIM/script/c101010087.lua
+++ CHIM/script/c101010087.lua
@@ -62,7 +62,7 @@ end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     return c:IsReason(REASON_BATTLE)
-        or (rp~=tp and c:IsReason(REASON_EFFECT) and c:GetPreviousControler()==tp)
+        or (rp==1-tp and c:IsReason(REASON_EFFECT) and c:GetPreviousControler()==tp)
 end
 function s.filter(c,e,tp)
     return c:IsSetCard(0x131) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
