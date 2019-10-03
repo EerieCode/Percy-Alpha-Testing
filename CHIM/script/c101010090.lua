3c01ee731c347a2981e7b27bcd11a8224cb3b42d
pyrQ <30827955+pyrQ@users.noreply.github.com>
2019-10-03 23:03:04 +0300

    Update c101010090.lua

--- CHIM/script/c101010090.lua
+++ CHIM/script/c101010090.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 end
 s.listed_names={CARD_DREAM_MIRROR_JOY, CARD_DREAM_MIRROR_TERROR }
 function s.filter(c,tp)
-    return c:IsType(TYPE_FIELD) and c:IsCode(CARD_DREAM_MIRROR_JOY,CARD_DREAM_MIRROR_TERROR) and c:GetActivateEffect():IsActivatable(tp)
+    return c:IsType(TYPE_FIELD) and c:IsCode(CARD_DREAM_MIRROR_JOY,CARD_DREAM_MIRROR_TERROR)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
@@ -30,16 +30,11 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
     local cs={tg1:GetFirst(),tg2:GetFirst()}
     for p=0,1 do
         local tc=cs[p]
-        local te=tc:GetActivateEffect()
-        local fc=Duel.GetFieldCardtp,LOCATION_SZONE,5)
+        local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
         if fc then
             Duel.SendtoGrave(fc,REASON_RULE)
             Duel.BreakEffect()
         end
         Duel.MoveToField(tc,tp,p,LOCATION_SZONE,POS_FACEUP,true)
-        local tep=tc:GetControler()
-        local cost=te:GetCost()
-        if cost then cost(te,tep,eg,ep,ev,re,r,rp,1) end
-        Duel.RaiseEvent(tc,4179255,te,0,p,tp,Duel.GetCurrentChain())
     end
 end
