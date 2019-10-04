864f5ac45401cde368ea83fc7dcd07db2b3a22b2
Sanity Sierota <47074795+that-hatter@users.noreply.github.com>
2019-10-04 21:12:14 +0800

    Update c101011067.lua

--- IGAS-JP/script/c101011067.lua
+++ IGAS-JP/script/c101011067.lua
@@ -3,11 +3,11 @@
 --scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	--rec or dam
+	--destroy
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
