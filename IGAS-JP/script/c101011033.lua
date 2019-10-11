4e74073c46b5f9324c60e710b54c1a12af23ffe0
AlphaKretin <deewhydeeecks@gmail.com>
2019-10-11 16:22:25 +1030

    Fix "Cupid Serve"
    gain 900 levels lmao

--- IGAS-JP/script/c101011033.lua
+++ IGAS-JP/script/c101011033.lua
@@ -41,7 +41,7 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_LEVEL)
-        e1:SetValue(ct*300)
+        e1:SetValue(ct)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END,2)
         c:RegisterEffect(e1)
     end
