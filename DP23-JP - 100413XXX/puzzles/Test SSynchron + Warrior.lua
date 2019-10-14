1f7b47b87b8e7dc7f221842957ab520ddad38a45
NaimSantos <naimsantos45@gmail.com>
2019-10-14 16:00:20 -0300

    New puzzles

---
+++ DP23-JP - 100413XXX/puzzles/Test SSynchron + Warrior.lua
@@ -0,0 +1,28 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Extra Deck
+Debug.AddCard(100413023,0,0,LOCATION_EXTRA,0,8)
+--Hand
+Debug.AddCard(19230407,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(100413025,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(83764718,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(19230407,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(19230407,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(19230407,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--Monster Zones
+Debug.AddCard(74586817,0,0,LOCATION_MZONE,0,1,true)
+Debug.AddCard(64880894,0,0,LOCATION_MZONE,4,1,true)
+Debug.AddCard(64880894,0,0,LOCATION_MZONE,2,1,true)
+Debug.AddCard(98780137,0,0,LOCATION_MZONE,3,4,true)
+--Monster Zones
+Debug.AddCard(69327790,1,1,LOCATION_MZONE,2,4,true)
+Debug.AddCard(69327790,1,1,LOCATION_MZONE,3,4,true)
+Debug.AddCard(69327790,1,1,LOCATION_MZONE,4,4,true)
+--Spell & Trap Zones
+Debug.AddCard(17626381,1,1,LOCATION_SZONE,3,5)
+Debug.AddCard(17626381,1,1,LOCATION_SZONE,0,5)
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
