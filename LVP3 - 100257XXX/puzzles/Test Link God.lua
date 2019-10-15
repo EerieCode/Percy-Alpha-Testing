4538e6b76c7d1e8d3f193f602e3f9e911ec1df8f
NaimSantos <naimsantos45@gmail.com>
2019-10-15 14:30:18 -0300

    Fixed L. G. D.

--- LVP3 - 100257XXX/puzzles/Test Link God.lua
+++ LVP3 - 100257XXX/puzzles/Test Link God.lua
@@ -15,6 +15,7 @@ Debug.AddCard(1799464,0,0,LOCATION_DECK,0,POS_FACEDOWN)
 Debug.AddCard(100257001,0,0,LOCATION_EXTRA,0,8)
 --Hand
 Debug.AddCard(83764718,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+Debug.AddCard(19230407,0,0,LOCATION_HAND,0,POS_FACEDOWN)
 --Monster Zones
 Debug.AddCard(3987233,0,0,LOCATION_MZONE,0,1,true)
 Debug.AddCard(11270236,0,0,LOCATION_MZONE,1,1,true)
@@ -29,4 +30,3 @@ Debug.AddCard(75878039,1,1,LOCATION_MZONE,3,4,true)
 --Spell & Trap Zones
 Debug.AddCard(17626381,1,1,LOCATION_SZONE,4,5)
 Debug.ReloadFieldEnd()
-aux.BeginPuzzle()
