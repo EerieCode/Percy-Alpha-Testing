7cbf5f57507fd1522f6df202a5f2976f7284610a
NaimSantos <naimsantos45@gmail.com>
2019-10-15 08:53:42 -0300

    Fixed string

--- LVP3 - 100257XXX/script/c100257071.lua
+++ LVP3 - 100257XXX/script/c100257071.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--copy spell
 	local e2=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id+100)
