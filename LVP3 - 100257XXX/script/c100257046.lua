45340eb7c204ebaae6ddf90a88eb0093380741ad
ahtelel <32773013+ahtelel@users.noreply.github.com>
2019-11-13 11:29:06 +0700

    Added Reptilianne Echidna (untested)

---
+++ LVP3 - 100257XXX/script/c100257046.lua
@@ -0,0 +1,78 @@
+--エキドゥーナ
+--Reptilianne Echidna
+--Scripted by ahtelel
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,nil,2,2,s.lcheck)
+	c:EnableReviveLimit()
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.tgcon)
+	e1:SetTarget(s.tgtg)
+	e1:SetOperation(s.tgop)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id+100)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+function s.lcheck(g,lc)
+	return g:IsExists(Card.IsLinkRace,1,nil,RACE_REPTILE)
+end
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsFaceup() and chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.nzatk,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,aux.nzatk,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(0)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.thfilter(c)
+	return c:IsRace(RACE_REPTILE) and c:IsAbleToHand()
+end
+function s.countfilter(c)
+	return c:IsFaceup() and c:IsAttack(0)
+end
+function s.threscon(sg,e,tp,mg)
+	return sg:GetClassCount(Card.GetCode)==#sg
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(s.countfilter,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if chk==0 then return ct>0 and aux.SelectUnselectGroup(g,e,tp,1,ct,s.threscon,0) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetMatchingGroupCount(s.countfilter,tp,0,LOCATION_MZONE,nil)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if not (ct>0 and #g>0) then return end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,ct,s.threscon,1,tp,HINTMSG_ATOHAND)
+	if #sg>0 then
+		Duel.SendtoHand(sg,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
