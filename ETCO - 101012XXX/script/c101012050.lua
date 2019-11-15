407dd701c683da802768f69db8354ecb4b6a8bc8
EerieCode <eeriecode@gmail.com>
2019-11-15 20:42:14 +0100

    Fixed "The Arrival Cyberse @Ignister"

--- ETCO - 101012XXX/script/c101012050.lua
+++ ETCO - 101012XXX/script/c101012050.lua
@@ -50,12 +50,15 @@ end
 function s.efilter(e,te)
 	return te:GetOwner()~=e:GetOwner()
 end
+function s.desfilter(c,hc,tp)
+	local zone=hc:GetLinkedZone(tp)
+	return Duel.GetMZoneCount(tp,c,tp,LOCATION_REASON_TOFIELD,zone)>0
+end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc~=e:GetHandler() end
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,c) 
-		and Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,c:GetLinkedZone())>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,CARD_IGNISTER_TOKEN,0x135,0x4011,0,0,1,RACE_CYBERSE,ATTRIBUTE_DARK) end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,c,c,tp) 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0x135,0x4011,0,0,1,RACE_CYBERSE,ATTRIBUTE_DARK) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,c)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
@@ -65,9 +68,9 @@ end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0
-		and Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,e:GetHandler():GetLinkedZone())>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,CARD_IGNISTER_TOKEN,0x135,0x4011,0,0,1,RACE_CYBERSE,ATTRIBUTE_DARK) then
-		local token=Duel.CreateToken(tp,CARD_IGNISTER_TOKEN)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,e:GetHandler():GetLinkedZone(tp))>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0x135,0x4011,0,0,1,RACE_CYBERSE,ATTRIBUTE_DARK) then
+		local token=Duel.CreateToken(tp,id+100)
 		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP,e:GetHandler():GetLinkedZone())
 	end
 end
