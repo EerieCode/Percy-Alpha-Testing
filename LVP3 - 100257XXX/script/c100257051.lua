99e50873e06aba4df988f5850322f2e5f1042735
EerieCode <eeriecode@gmail.com>
2019-11-15 07:23:36 +0100

    Fixed "Mecha Phantom Beast Auroradon"

--- LVP3 - 100257XXX/script/c100257051.lua
+++ LVP3 - 100257XXX/script/c100257051.lua
@@ -24,7 +24,6 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id+100)
-	e2:SetCost(s.cost)
 	e2:SetTarget(s.tg)
 	e2:SetOperation(s.op)
 	c:RegisterEffect(e2)
@@ -35,18 +34,18 @@ end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>2
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,31533705,0x101b,TYPES_TOKEN,0,0,3,RACE_MACHINE,ATTRIBUTE_WIND) end
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0x101b,TYPES_TOKEN,0,0,3,RACE_MACHINE,ATTRIBUTE_WIND) end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,3,tp,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,3,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1 
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,31533705,0x101b,TYPES_TOKEN,0,0,3,RACE_MACHINE,ATTRIBUTE_WIND) then
-		local token1=Duel.CreateToken(tp,id+1)
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+100,0x101b,TYPES_TOKEN,0,0,3,RACE_MACHINE,ATTRIBUTE_WIND) then
+		local token1=Duel.CreateToken(tp,id+100)
 		Duel.SpecialSummonStep(token1,0,tp,tp,false,false,POS_FACEUP)
-		local token2=Duel.CreateToken(tp,id+1)
+		local token2=Duel.CreateToken(tp,id+100)
 		Duel.SpecialSummonStep(token2,0,tp,tp,false,false,POS_FACEUP)
-		local token3=Duel.CreateToken(tp,id+1)
+		local token3=Duel.CreateToken(tp,id+100)
 		Duel.SpecialSummonStep(token3,0,tp,tp,false,false,POS_FACEUP)
 		Duel.SpecialSummonComplete()
 	end
@@ -60,7 +59,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.RegisterEffect(e1,tp)
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,2))
 	e2:SetReset(RESET_PHASE+PHASE_END)
 	e2:SetTargetRange(1,0)
 	Duel.RegisterEffect(e2,tp)
@@ -74,41 +73,36 @@ end
 function s.svfilter(c,tp)
 	return c:IsType(TYPE_TRAP) and c:IsAbleToHand()
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,aux.TRUE,1,false,nil,e:GetHandler()) end
-	local g=Duel.SelectReleaseGroupCost(tp,aux.TRUE,1,3,false,nil,e:GetHandler())
-	Duel.Release(g,REASON_COST)
-	e:SetLabel(#g)
-end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	local eff=e:GetLabel()
-	if chk==0 then
-		local ct=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		local sel=0
-		if eff==1 and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)) then sel=0 end
-		if ct>0 and eff==2 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil) then sel=sel+1 end
-		if eff==3 and Duel.IsExistingMatchingCard(s.svfilter,tp,LOCATION_GRAVE,0,1,nil) then sel=sel+2 end
-		e:SetLabel(sel)
-		return sel~=0 and true
+	local b1=Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) and Duel.CheckReleaseGroupCost(tp,aux.TRUE,1,false,nil,nil)
+	local b2=Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) and Duel.CheckReleaseGroupCost(tp,aux.TRUE,2,false,nil,nil)
+	local b3=Duel.IsExistingMatchingCard(s.svfilter,tp,LOCATION_GRAVE,0,1,nil) and Duel.CheckReleaseGroupCost(tp,aux.TRUE,3,false,nil,nil)
+	if chk==0 then return b1 or b2 or b3 end
+	local stab={}
+	local dtab={}
+	if b1 then
+		table.insert(stab,1)
+		table.insert(dtab,aux.Stringid(id,3))
 	end
-	local sel=e:GetLabel()
-	if sel==2 then
-		e:SetCategory(CATEGORY_LEAVE_GRAVE)
-		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
-	elseif sel==1 then
-		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-	elseif sel==0 then
-		local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	if b2 then
+		table.insert(stab,2)
+		table.insert(dtab,aux.Stringid(id,4))
 	end
+	if b3 then
+		table.insert(stab,3)
+		table.insert(dtab,aux.Stringid(id,5))
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EFFECT)
+	local op=Duel.SelectOption(tp,table.unpack(dtab))+1
+	local sel=stab[op]
+	local g=Duel.SelectReleaseGroupCost(tp,aux.TRUE,sel,sel,false,nil,nil)
+	Duel.Release(g,REASON_COST)
 	e:SetLabel(sel)
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local sel=e:GetLabel()
-		if sel==0 then
+		if sel==1 then
 			local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 			if #g>0 then
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
@@ -116,11 +110,11 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 				Duel.HintSelection(dg)
 				Duel.Destroy(dg,REASON_EFFECT)
 		end
-		elseif sel==1 and not Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then
+		elseif sel==2 and not Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 			local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
 			if tc then Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-		elseif sel==0 then
+		elseif sel==3 then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 			local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.svfilter),tp,LOCATION_GRAVE,0,1,1,nil)
 			if #g>0 then
