80cbfaf74a28d9aebeb0f0a2a7623a70b0f5ea98
ahtelel <32773013+ahtelel@users.noreply.github.com>
2019-11-16 02:52:09 +0700

    Fix Splash Mage

--- ETCO - 101012XXX/script/c101012048.lua
+++ ETCO - 101012XXX/script/c101012048.lua
@@ -31,6 +31,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
 	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
