4b6fa376ba4a8d0702a0eb4d3449724f931ecfab
NaimSantos <naimsantos45@gmail.com>
2019-12-12 15:14:39 -0300

    Test: "White Mirror"

---
+++ 20PP - 100229XXX/puzzles/Test White Mirror.lua
@@ -0,0 +1,17 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+--Main Deck
+Debug.AddCard(37953640,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(9126351,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+Debug.AddCard(37953640,0,0,LOCATION_DECK,0,POS_FACEDOWN)
+--Hand
+Debug.AddCard(100229018,0,0,LOCATION_HAND,0,POS_FACEDOWN)
+--GY
+Debug.AddCard(9126351,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+Debug.AddCard(9126351,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+Debug.AddCard(37953640,0,0,LOCATION_GRAVE,0,POS_FACEUP)
+Debug.ReloadFieldEnd()
+aux.BeginPuzzle()
