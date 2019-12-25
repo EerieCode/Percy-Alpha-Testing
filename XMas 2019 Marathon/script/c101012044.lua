b48418772ec0c303b86d3ffca9b779bfc37d923e
AlphaKretin <deewhydeeecks@gmail.com>
2019-12-25 14:25:06 +1030

    Complete "Valiant Shark Lancer"

--- XMas 2019 Marathon/script/c101012044.lua
+++ XMas 2019 Marathon/script/c101012044.lua
@@ -1,18 +1,17 @@
---
+--ヴァリアント・シャーク・ランサー
 --Valiant Shark Lancer
 --Scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
 	--destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(aux.NOT(s.quickcon))
-	e1:SetCost(s.descost)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
@@ -21,6 +20,18 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetCondition(s.quickcon)
 	c:RegisterEffect(e2)
+	--topdeck
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_DRAW)
+    e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetCountLimit(1,id+100)
+    e3:SetCondition(s.tdcon)
+    e3:SetTarget(s.tdtg)
+    e3:SetOperation(s.tdop)
+    c:RegisterEffect(e3)
 end
 function s.cfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_WATER)
@@ -28,21 +39,36 @@ end
 function s.quickcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,e:GetHandler())
 end
-function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	c:RemoveOverlayCard(tp,1,1,REASON_COST)
-end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
-	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) 
+		and Duel.CheckRemoveOverlayCard(tp,LOCATION_MZONE,0,1,REASON_EFFECT) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
+	if Duel.RemoveOverlayCard(tp,LOCATION_MZONE,0,1,1,REASON_EFFECT) and tc:IsRelateToEffect(e) then
 		Duel.Destroy(tc,REASON_EFFECT)
 	end
+end
+function s.tdcfilter(c,tp)
+    return c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousControler()==tp
+    	and c:IsType(TYPE_XYZ) and c:IsAttribute(ATTRIBUTE_WATER)
+end
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+    return eg:IsExists(s.tdcfilter,1,nil,tp)
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_DECK,0,1,nil,TYPE_SPELL) end
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+    local tc=Duel.SelectMatchingCard(tp,Card.IsType,tp,LOCATION_DECK,0,1,1,nil,TYPE_SPELL):GetFirst()
+    if tc then
+        Duel.ShuffleDeck(tp)
+        Duel.MoveSequence(tc,0)
+        Duel.ConfirmDecktop(tp,1)
+    end
 end
