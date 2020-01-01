1ebe9e704b5fe48f2ee4171131e6001d1ba6fdf5
EerieCode <eeriecode@gmail.com>
2020-01-01 12:38:38 +0100

    Added cdb and puzzle

---
+++ ETCO - 101012XXX/ETCO - Union.lua
@@ -0,0 +1,13 @@
+--Created using senpaizuri's Puzzle Maker (updated by Naim & Larry126)
+Debug.ReloadFieldBegin(DUEL_ATTACK_FIRST_TURN+DUEL_SIMPLE_AI,4)
+Debug.SetPlayerInfo(0,8000,0,0)
+Debug.SetPlayerInfo(1,8000,0,0)
+
+Debug.AddCard(11321183,0,0,LOCATION_MZONE,0,1,true)
+Debug.AddCard(101012034,0,0,LOCATION_MZONE,2,1,true)
+
+Debug.AddCard(47415292,0,0,LOCATION_DECK,2,POS_FACEDOWN,true)
+Debug.AddCard(7369217,0,0,LOCATION_DECK,2,POS_FACEDOWN,true)
+Debug.AddCard(47693640,0,0,LOCATION_DECK,2,POS_FACEDOWN,true)
+
+Debug.ReloadFieldEnd()
