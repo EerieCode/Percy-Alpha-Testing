9df8631b8fc8d2edd762b16641b8af1054dd595e
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-01-01 23:44:54 -0500

    Updated "Ravenous Crocosaur Archetis"
    Made the following changes:
    * Trigger effect should only happen if synchro summoned
    * Reworked trigger effect
    * Should be able to target itself with its quick effect

--- ETCO - 101012XXX/script/c101012043.lua
+++ ETCO - 101012XXX/script/c101012043.lua
@@ -5,12 +5,6 @@ local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
     aux.AddSynchroProcedure(c,nil,1,1,aux.NonTuner(nil),1,99)
-    --matcheck
-    local e0=Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_SINGLE)
-    e0:SetCode(EFFECT_MATERIAL_CHECK)
-    e0:SetValue(s.valcheck)
-    c:RegisterEffect(e0)
     --draw
     local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_DRAW)
@@ -18,7 +12,7 @@ function s.initial_effect(c)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
     e1:SetCountLimit(1,id)
-    e1:SetLabelObject(e0)
+    e1:SetCondition(s.drcon)
     e1:SetTarget(s.drtg)
     e1:SetOperation(s.drop)
     c:RegisterEffect(e1)
@@ -47,12 +41,11 @@ function s.initial_effect(c)
     e4:SetOperation(s.desop)
     c:RegisterEffect(e4)
 end
-function s.valcheck(e,c)
-    e:SetLabel(c:GetMaterialCount()-1)
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    local c=e:GetHandler()
-    local ct=e:GetLabel()
+    local ct=(e:GetHandler():GetMaterialCount())-1
     if chk==0 then return ct>0 and Duel.IsPlayerCanDraw(tp,ct) end
     Duel.SetTargetPlayer(tp)
     Duel.SetTargetParam(ct)
@@ -70,10 +63,10 @@ function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsOnField() and chkc~=e:GetHandler() end
-    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+    if chkc then return chkc:IsOnField() end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
     Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
