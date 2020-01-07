1d41b5f348d02274189d1508e0ec3d909b0d9e44
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-01-07 17:52:18 +0200

    Update c101012058.lua

--- ETCO - 101012XXX/script/c101012058.lua
+++ ETCO - 101012XXX/script/c101012058.lua
@@ -13,15 +13,12 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.tpfilter(c)
-	return c:IsType(TYPE_LINK)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return false end
-    if chk==0 then return Duel.IsExistingTarget(s.tpfilter,tp,LOCATION_MZONE,0,1,nil)
+    if chk==0 then return Duel.IsExistingTarget(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_LINK)
         and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELF)
-    local g1=Duel.SelectTarget(tp,s.tpfilter,tp,LOCATION_MZONE,0,1,1,nil)
+    local g1=Duel.SelectTarget(tp,Card.IsType,tp,LOCATION_MZONE,0,1,1,nil,TYPE_LINK)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPPO)
     local g2=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
     local g=g1+g2
@@ -31,9 +28,9 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
     local g=Duel.GetTargetCards(e)
     if #g>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.BreakEffect()
         Duel.Draw(p,d,REASON_EFFECT)
     end
