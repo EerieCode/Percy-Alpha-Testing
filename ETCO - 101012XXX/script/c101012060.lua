030e8d46a93ae220a4d581816fc2d1b71a10eed0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-01-08 16:21:10 +0200

    "Nemesis Adrastia" fix
    The target must be face-up

--- ETCO - 101012XXX/script/c101012060.lua
+++ ETCO - 101012XXX/script/c101012060.lua
@@ -16,10 +16,10 @@ function s.initial_effect(c)
 end
 s.listed_series={0x23d}
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x23d) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsFaceup() and c:IsSetCard(0x23d) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
