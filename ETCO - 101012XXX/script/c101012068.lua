a8c1ceca118abf881f85faf66145f235378ebb63
NaimSantos <naimsantos45@gmail.com>
2020-01-08 08:36:41 -0300

    script

--- ETCO - 101012XXX/script/c101012068.lua
+++ ETCO - 101012XXX/script/c101012068.lua
@@ -1,21 +1,24 @@
---
+--眷現の呪眼
 --Evil Eye Manifestation
 --Scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
 	e1:SetTarget(s.tktg)
 	e1:SetOperation(s.tkop)
 	c:RegisterEffect(e1)
 	--target
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,2))
+    e2:SetDescription(aux.Stringid(id,1))
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id+100)
     e2:SetCost(aux.bfgcost)
     e2:SetOperation(s.tgop)
     c:RegisterEffect(e2)
@@ -46,7 +49,7 @@ function s.tkop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
@@ -58,7 +61,7 @@ end
 function s.tgop(e,tp,eg,ep,ev,re,r,rp)
     --cannot be target
     local e1=Effect.CreateEffect(e:GetHandler())
-    e1:SetDescription(aux.Stringid(id,1))
+    e1:SetDescription(aux.Stringid(id,3))
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_CLIENT_HINT)
     e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
