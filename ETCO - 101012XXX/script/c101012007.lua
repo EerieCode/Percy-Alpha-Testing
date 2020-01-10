16c4c5e91f2b789ced07c810d9691dc51a1ee09b
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-01-10 22:10:13 +0200

    Update c101012007.lua

--- ETCO - 101012XXX/script/c101012007.lua
+++ ETCO - 101012XXX/script/c101012007.lua
@@ -90,7 +90,7 @@ end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
