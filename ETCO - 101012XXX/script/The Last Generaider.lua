9dfc886a98b094415724a6e63d632078e6bf58e7
ahtelel <32773013+ahtelel@users.noreply.github.com>
2020-01-10 10:47:40 +0700

    Finalized The Last Generaider

--- ETCO - 101012XXX/script/The Last Generaider.lua
+++ ETCO - 101012XXX/script/The Last Generaider.lua
@@ -1,5 +1,5 @@
---
---
+--轟の王ハール
+--Hoárr, Generaider Boss of Rumbling
 --Scripted by ahtelel
 local s,id=GetID()
 function s.initial_effect(c)
@@ -16,45 +16,19 @@ function s.initial_effect(c)
 	e1:SetCondition(s.scon0)
 	e1:SetOperation(s.sop0)
 	c:RegisterEffect(e1)
-	--send gy (setzen: triggers when 1-tp adds a card from deck to hand, except by being drawn)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
-	e1:SetCode(EVENT_TO_HAND)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,EFFECT_COUNT_CODE_SINGLE)
-	e1:SetCondition(s.scon1)
-	e1:SetOperation(s.sop1)
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
-	e2:SetCode(EVENT_TO_HAND)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCondition(s.regcon)
-	e2:SetOperation(s.regop)
-	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_CHAIN_SOLVED)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,EFFECT_COUNT_CODE_SINGLE)
-	e3:SetCondition(s.scon2)
-	e3:SetOperation(s.sop1)
-	c:RegisterEffect(e3)
 	--negate
-	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetCode(EVENT_CHAINING)
-	e4:SetCountLimit(1,id)
-	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCondition(s.discon)
-	e4:SetTarget(s.distg)
-	e4:SetOperation(s.disop)
-	c:RegisterEffect(e4)
---sanct's condition
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCountLimit(1,id)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.discon)
+	e2:SetTarget(s.distg)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+end
 function s.cfilter(c,tp)
 	return c:IsControler(tp) and c:IsPreviousLocation(LOCATION_DECK)
 end
@@ -62,32 +36,7 @@ function s.scon0(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()~=PHASE_DRAW and eg:IsExists(s.cfilter,1,nil,1-tp)
 		and Duel.GetFieldGroupCount(1-tp,0,LOCATION_ONFIELD+LOCATION_HAND)>0
 end
---setzen's condition
-function s.cfilter(c,tp)
-	return c:IsControler(1-tp) and not c:IsReason(REASON_DRAW) and c:IsPreviousLocation(LOCATION_DECK)
-end
-function s.scon1(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp) 
-		and (not re:IsHasType(EFFECT_TYPE_ACTIONS) or re:IsHasType(EFFECT_TYPE_CONTINUOUS))
-end
-function s.regcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,nil,tp) and Duel.GetFlagEffect(tp,id)==0 
-		and re:IsHasType(EFFECT_TYPE_ACTIONS) and not re:IsHasType(EFFECT_TYPE_CONTINUOUS)
-end
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(tp,id,RESET_CHAIN,0,1)
-end
-function s.scon2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,id)>0
-end
---sanct's operation: 1-tp must send any card to gy
 function s.sop0(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(1-tp,aux.TRUE,1-tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil)
-	if #g>0 then Duel.SendtoGrave(g,REASON_RULE) end
-end
---setzen's operation: 1-tp must send a monster only to gy
-function s.sop1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(1-tp,Card.IsType,1-tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil,TYPE_MONSTER)
 	if #g>0 then Duel.SendtoGrave(g,REASON_RULE) end
