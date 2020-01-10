85d1c0ec4e7276e3ba5da76069e15301b119a66e
NaimSantos <naimsantos45@gmail.com>
2020-01-10 13:58:35 -0300

    Fixed "Marincess Great Bubble Reef"

--- ETCO - 101012XXX/script/c101012054.lua
+++ ETCO - 101012XXX/script/c101012054.lua
@@ -49,7 +49,7 @@ end
 function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.drcftiler,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(s.drcfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.drcfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
