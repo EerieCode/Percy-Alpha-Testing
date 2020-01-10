6de4b15b3dfa61245b617d5cbc3ce9d11ffca916
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-01-10 22:06:59 +0200

    Update c101012053.lua

--- ETCO - 101012XXX/script/c101012053.lua
+++ ETCO - 101012XXX/script/c101012053.lua
@@ -43,7 +43,7 @@ function s.efilter(e,te)
 	return te:GetOwner()~=e:GetOwner()
 end
 function s.atkval(e,c)
-	local g=Duel.GetMatchingGroup(Card.IsType,c:GetControler(),LOCATION_MZONE,LOCATION_MZONE,c,TYPE_LINK)
+	local g=Duel.GetMatchingGroup(Card.IsType,c:GetControler(),LOCATION_MZONE,0,c,TYPE_LINK)
 	return g:GetSum(Card.GetLink)*200
 end
 function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -65,7 +65,7 @@ function s.cfilter(c)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
-	local dg=Duel.GetMatchingGroup(Card.IsCanBeEffectTarget,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler(),e)
+	local dg=Duel.GetMatchingGroup(Card.IsCanBeEffectTarget,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,e)
 	if chk==0 then
 		if e:GetLabel()~=100 then return false end
 		e:SetLabel(0)
@@ -75,7 +75,7 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local lnk=sg:GetFirst():GetLink()
 	Duel.Release(sg,REASON_COST)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local dg=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,lnk,lnk,e:GetHandler())
+	local dg=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,lnk,lnk,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,#dg,tp,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
