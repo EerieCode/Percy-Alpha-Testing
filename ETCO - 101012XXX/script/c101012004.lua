09282b8dd493619425ac2f007e1cdfc89e922acf
NaimSantos <naimsantos45@gmail.com>
2020-01-10 10:07:59 -0300

    Fixed "Gouki Iron Claw"

--- ETCO - 101012XXX/script/c101012004.lua
+++ ETCO - 101012XXX/script/c101012004.lua
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--tohand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(60461077,1))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
