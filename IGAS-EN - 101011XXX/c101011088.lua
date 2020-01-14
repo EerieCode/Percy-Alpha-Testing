e67bcc82b49d72782024dc2d5c2457fcb0fb24a7
Larry126 <larry126@users.noreply.github.com>
2020-01-14 23:48:43 +0800

    Added missing Xyz proc

--- IGAS-EN - 101011XXX/c101011088.lua
+++ IGAS-EN - 101011XXX/c101011088.lua
@@ -3,6 +3,9 @@
 --Scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
+	--xyz summon
+	aux.AddXyzProcedure(c,nil,4,2)
+	c:EnableReviveLimit()
 	--banish
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
