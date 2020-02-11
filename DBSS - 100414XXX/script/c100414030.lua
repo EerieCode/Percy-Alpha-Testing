1763d6f20885d2322f00f620354e6a8051bfe161
AlphaKretin <deewhydeeecks@gmail.com>
2020-02-11 15:07:10 +1030

    Fix "Edllixirs"
    Defense position

--- DBSS - 100414XXX/script/c100414030.lua
+++ DBSS - 100414XXX/script/c100414030.lua
@@ -27,7 +27,7 @@ function s.eldicheck(tp)
 end
 function s.spfilter(c,e,tp)
     return (Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x23f),tp,LOCATION_MZONE,0,1,nil) 
-    	or c:IsSetCard(0x23f)) and c:IsRace(RACE_ZOMBIE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+    	or c:IsSetCard(0x23f)) and c:IsRace(RACE_ZOMBIE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -39,7 +39,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
     local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp)
     if #g>0 then
-        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
     end
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetDescription(aux.Stringid(id,2))
