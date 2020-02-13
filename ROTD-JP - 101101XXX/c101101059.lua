a5a7717e78b86f4b5aa56e3e07aab211038ec00f
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-02-13 16:23:58 +0200

    "Magallanica, the Deep Sea City" fixes/updates

--- ROTD-JP - 101101XXX/c101101059.lua
+++ ROTD-JP - 101101XXX/c101101059.lua
@@ -21,6 +21,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 	--banish
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,3))
 	e3:SetCategory(CATEGORY_REMOVE)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -56,22 +57,23 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		local lv=Duel.SelectOption(tp,aux.Stringid(id,3),aux.Stringid(id,4))
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,4))
+		local lv=Duel.AnnounceLevel(tp,1,2)
 		local e1=Effect.CreateEffect(c)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_LEVEL)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e1:SetValue(lv+1)
+		e1:SetValue(lv)
 		tc:RegisterEffect(e1)
 	end
 end
 function s.rmcfilter(c,tp)
-	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:GetSummonPlayer()==tp
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsType(TYPE_SYNCHRO) and c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:GetSummonPlayer()==tp
 end
 function s.rmcon(e,tp,eg,ep,ev,re,r,rp)
 	local ph=Duel.GetCurrentPhase()
-	return (ph==PHASE_MAIN1 or ph==PHASE_MAIN2) and Duel.GetTurnPlayer()==tp and eg:isExists(s.rmcfilter,1,nil,tp)
+	return (ph==PHASE_MAIN1 or ph==PHASE_MAIN2) and Duel.GetTurnPlayer()==tp and eg:IsExists(s.rmcfilter,1,nil,tp)
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_HAND,1,nil) end
