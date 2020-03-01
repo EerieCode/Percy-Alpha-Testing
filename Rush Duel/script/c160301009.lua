6cd1f76994a79a3ee86d4ecb5dbc446feee27443
ClementLouis <clementlouis.gallier@viacesi.fr>
2020-03-01 16:06:20 +0100

    added 3 additional Rush cards
    added Luminous Shaman, Counteroffensive Dragonstrike and Dragonic Pressure

---
+++ Rush Duel/script/c160301009.lua
@@ -0,0 +1,37 @@
+--Luminous Shaman
+local s,id=GetID()
+function s.initial_effect(c)
+	--direct attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.dircost)
+	e1:SetTarget(s.dirtg)
+	e1:SetOperation(s.dirop)
+	c:RegisterEffect(e1)
+end
+function s.dircost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+end
+function s.filter(c)
+	return c:IsFaceup() and not c:IsHasEffect(EFFECT_DIRECT_ATTACK)
+end
+function s.dirtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local dg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return #dg>0 end
+end
+function s.dirop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local dg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_ONFIELD,nil)
+	if #dg>0 and not c:IsRelateToEffect(e) then return end
+	local sg=dg:Select(tp,1,1,nil)
+	local tc=sg:GetFirst()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_EQUIP)
+	e1:SetProperty(EFFECT_CANNOT_DISABLE)
+	e1:SetCode(EFFECT_DIRECT_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	tc:RegisterEffect(e1)
+end
