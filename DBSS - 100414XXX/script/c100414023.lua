96a0902d67de6e3f58b1d2fd08598effb9260de2
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-03-03 23:54:35 +0200

    "Stunning Snowflower" fix + update

--- DBSS - 100414XXX/script/c100414023.lua
+++ DBSS - 100414XXX/script/c100414023.lua
@@ -1,6 +1,6 @@
 --六花絢爛
 --Stunning Snowflower
---scripted by Naim
+--scripted by Naim, updated by pyrQ
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -11,50 +11,44 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation1)
+	e1:SetOperation(s.operation2)
+	e1:SetLabel(0)
 	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCost(s.cost)
-	e2:SetOperation(s.operation2)
-	c:RegisterEffect(e2)
 end
 s.listed_series={0x244}
-function s.filter(c)
+function s.filter(c,e,tp,check)
 	return c:IsSetCard(0x244) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
-end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(tp,Card.IsRace,1,nil,RACE_PLANT) end
-	local g=Duel.SelectReleaseGroup(tp,Card.IsRace,1,1,nil,RACE_PLANT)
-	Duel.Release(g,REASON_COST)
+		and (check==0 or Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,c:GetOriginalLevel(),c:GetCode()))
 end
 function s.filter2(c,lv,code)
-	return c:IsRace(RACE_PLANT) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and not c:GetLevel()==lv and not c:GetCode()==code
+	return c:IsRace(RACE_PLANT) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand() and c:GetOriginalLevel()==lv and not c:IsCode(code)
 end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp,0)
+	local b=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp,1)
+	if chk==0 then return a or b end
+	if b and Duel.CheckReleaseGroupCost(tp,Card.IsRace,1,false,nil,nil,RACE_PLANT)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		local g=Duel.SelectReleaseGroupCost(tp,Card.IsRace,1,1,false,nil,nil,RACE_PLANT)
+		Duel.Release(g,REASON_COST)
+		e:SetLabel(1)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+	else
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		local lv=g:GetFirst():GetLevel()
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp,e:GetLabel())
+	if #g>0 then
+		local lv=g:GetFirst():GetOriginalLevel()
 		local code=g:GetFirst():GetCode()
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
-		if Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,lv,code) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
-			Duel.BreakEffect()
+		if e:GetLabel()==1 then
 			local sg=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil,lv,code)
-			if sg:GetCount()>0 then
+			if #sg>0 then
+				Duel.BreakEffect()
 				Duel.SendtoHand(sg,nil,REASON_EFFECT)
 				Duel.ConfirmCards(1-tp,sg)
 			end
