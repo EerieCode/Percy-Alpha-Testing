075f2da1b3b01e101f0150abc5db7ee77849b85e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-03-03 23:54:51 +0200

    Update c100414023.lua

--- DBSS - 100414XXX/script/c100414023.lua
+++ DBSS - 100414XXX/script/c100414023.lua
@@ -11,7 +11,7 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.operation2)
+	e1:SetOperation(s.operation)
 	e1:SetLabel(0)
 	c:RegisterEffect(e1)
 end
