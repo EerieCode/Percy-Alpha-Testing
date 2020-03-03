d476cd667cd910a413e59869b0022cba1a40f8fb
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-03-03 23:17:41 +0200

    "Snowflower Sprite Helleborus" fixes

--- DBSS - 100414XXX/script/c100414020.lua
+++ DBSS - 100414XXX/script/c100414020.lua
@@ -3,20 +3,20 @@
 --Scripted by AlphaKretin
 local s,id=GetID()
 function s.initial_effect(c)
-	--negate
-    local e1=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_DISABLE)
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_CHAINING)
-    e1:SetRange(LOCATION_MZONE+LOCATION_HAND)
-    e1:SetCountLimit(1,id)
-    e1:SetCondition(s.discon)
-    e1:SetCost(s.discost)
-    e1:SetTarget(s.distg)
-    e1:SetOperation(s.disop)
-    c:RegisterEffect(e1)
-    --special summon
+	--Negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_MZONE+LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.discon)
+	e1:SetCost(s.discost)
+	e1:SetTarget(s.distg)
+	e1:SetOperation(s.disop)
+	c:RegisterEffect(e1)
+	--Special Summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -28,14 +28,15 @@ function s.initial_effect(c)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
+s.listed_series={0x244}
 function s.cfilter(c,tp)
-    return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
 end
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
-    if not (rp==1-tp and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)) then return false end
-    local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-    return g and g:IsExists(s.cfilter,1,nil,tp) and Duel.IsChainNegatable(ev) and re:IsActivateType(TYPE_MONSTER)
-    	and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x244),tp,LOCATION_MZONE,0,1,nil)
+	if not (rp==1-tp and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET)) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return g and g:IsExists(s.cfilter,1,nil,tp) and Duel.IsChainDisablable(ev) and re:IsActiveType(TYPE_MONSTER)
+		and Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x244),tp,LOCATION_MZONE,0,1,nil)
 end
 function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
@@ -49,20 +50,18 @@ function s.disop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateEffect(ev)
 end
 function s.cfilter2(c,ft,tp)
-    return c:IsFaceup() and c:IsRace(RACE_PLANT)
-        and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5))
+	return c:IsRace(RACE_PLANT) and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5))
 end
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-    if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,s.cfilter2,1,false,nil,nil,ft,tp) end
-    local g=Duel.SelectReleaseGroupCost(tp,s.cfilter2,1,1,false,nil,nil,ft,tp)
-    Duel.Release(g,REASON_COST)
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,s.cfilter2,1,false,nil,nil,ft,tp) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter2,1,1,false,nil,nil,ft,tp)
+	Duel.Release(g,REASON_COST)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
-	end
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
