eaf399bbb46b805b9a6aa1893ba3715a75f9dc21
pyrQ <30827955+pyrQ@users.noreply.github.com>
2020-03-10 21:34:57 +0200

    "Merfae Hide-and-Seek" fixes

--- ROTD-JP - 101101XXX/c101101057.lua
+++ ROTD-JP - 101101XXX/c101101057.lua
@@ -1,32 +1,31 @@
 --メルフィーのかくれんぼ
---Melfie Hide and Seek
+--Merfae Hide-and-Seek
 --Scripted by ahtelel
 local s,id=GetID()
 function s.initial_effect(c)
-	function s.initial_effect(c)
-	--indes
+	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetTargetRange(LOCATION_MZONE,0)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_BEAST))
-	e1:SetValue(s.indct)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--draw
+	--Cannot be destroyed by card effects
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
 	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetTarget(s.drtg)
-	e2:SetOperation(s.drop)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_BEAST))
+	e2:SetValue(s.indct)
 	c:RegisterEffect(e2)
-	--activate
+	--Shuffle and draw
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_ACTIVATE)
-	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1,id)
+	e3:SetTarget(s.drtg)
+	e3:SetOperation(s.drop)
 	c:RegisterEffect(e3)
 end
 function s.indct(e,re,r,rp)
@@ -37,17 +36,14 @@ end
 function s.tdfilter(c)
 	return c:IsRace(RACE_BEAST) and c:IsAbleToDeck()
 end
+function s.tdcheck(sg,e,tp)
+    return sg:GetClassCount(Card.GetCode)==#sg
+end
 function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.tdfilter(chkc) end
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and g:GetClassCount(Card.GetCode)>2 end
-	local tg=Group.CreateGroup()
-	for i=1,3 do
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-		local sg=g:Select(tp,1,1,nil)
-		tg:Merge(sg)
-		g:Remove(Card.IsCode,nil,sg:GetFirst():GetCode())
-	end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and aux.SelectUnselectGroup(g,e,tp,3,3,s.tdcheck,0) end
+	local tg=aux.SelectUnselectGroup(g,e,tp,3,3,s.tdcheck,1,tp,HINTMSG_TODECK)
 	Duel.SetTargetCard(tg)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,tg,#tg,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
