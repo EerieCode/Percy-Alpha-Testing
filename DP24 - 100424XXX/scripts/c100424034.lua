854cc7ac0a279a5bce4c874c109497c40ed9ddfc
EerieCode <eeriecode@gmail.com>
2020-03-11 19:54:50 +0100

    Bug fixes

--- DP24 - 100424XXX/scripts/c100424034.lua
+++ DP24 - 100424XXX/scripts/c100424034.lua
@@ -39,13 +39,14 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 	--Destroy 1 card on the field, then if it was a trap, can apply 1 of 2 effects
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
 		local dg=Duel.GetMatchingGroup(aux.disfilter1,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 		if tc:IsType(TYPE_TRAP) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 			Duel.BreakEffect()
 			local op=0
-			local b1=Duel.IsExistingMatchingCard(aux.disfilter1,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+			local b1=(#dg>0)
 			local b2=Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 			Duel.Hint(HINT_SELECTMSG,tp,0)
 			if b1 and b2 then op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
@@ -53,31 +54,27 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			elseif b2 then Duel.SelectOption(tp,aux.Stringid(id,2)) op=1
 			else return end
 			if op==0 then --Negate 1 face-up card until end phase
-				local sg=dg:Select(tp,1,1,nil)
-				local tc=sg:GetFirst()
-				while tc do
-					Duel.NegateRelatedChain(tc,RESET_TURN_SET)
-					local e1=Effect.CreateEffect(e:GetHandler())
-					e1:SetType(EFFECT_TYPE_SINGLE)
-					e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-					e1:SetCode(EFFECT_DISABLE)
-					e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-					tc:RegisterEffect(e1)
-					local e2=Effect.CreateEffect(c)
-					e2:SetType(EFFECT_TYPE_SINGLE)
-					e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-					e2:SetCode(EFFECT_DISABLE_EFFECT)
-					e2:SetValue(RESET_TURN_SET)
-					e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-					tc:RegisterEffect(e2)
-					if tc:IsType(TYPE_TRAPMONSTER) then
-						local e3=Effect.CreateEffect(c)
-						e3:SetType(EFFECT_TYPE_SINGLE)
-						e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-						e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
-						e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-						tc:RegisterEffect(e3)
-					end
+				local sc=dg:Select(tp,1,1,nil):GetFirst()
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e1:SetCode(EFFECT_DISABLE)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				sc:RegisterEffect(e1)
+				local e2=Effect.CreateEffect(c)
+				e2:SetType(EFFECT_TYPE_SINGLE)
+				e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+				e2:SetCode(EFFECT_DISABLE_EFFECT)
+				e2:SetValue(RESET_TURN_SET)
+				e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				sc:RegisterEffect(e2)
+				if sc:IsType(TYPE_TRAPMONSTER) then
+					local e3=Effect.CreateEffect(c)
+					e3:SetType(EFFECT_TYPE_SINGLE)
+					e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+					e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+					e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+					sc:RegisterEffect(e3)
 				end
 			else --All "Jinzo" you control gains 800 ATK
 				local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,0,nil)
