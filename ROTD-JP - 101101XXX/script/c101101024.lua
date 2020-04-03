f1493043e5d717f74d208b12599a2da2cde804bf
NaimSantos <naimsantos45@gmail.com>
2020-04-03 09:38:39 -0300

    Added client hint call

--- ROTD-JP - 101101XXX/script/c101101024.lua
+++ ROTD-JP - 101101XXX/script/c101101024.lua
@@ -55,6 +55,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(s.actlimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
+	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,1),nil)
 end
 function s.actlimit(e,re,tp)
 	local rc=re:GetHandler()
