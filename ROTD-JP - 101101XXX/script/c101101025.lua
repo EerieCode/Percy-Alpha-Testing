b71aaecd11797de38bff1596e0e7d97125a6a7ef
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-02 23:48:22 -0400

    Added "Darklord Nergal"

---
+++ ROTD-JP - 101101XXX/script/c101101025.lua
@@ -0,0 +1,74 @@
+--堕天使ネルガル
+--Darklord Nergal
+--Logical Nonsense
+
+--Substitute ID
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetSPSummonOnce(id)
+	--Grants all the player's "Darklord" monsters piercing damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_PIERCE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xef))
+	c:RegisterEffect(e1)
+	--Pay 1000 LP; copy the effect of 1 "Darklord" S/T, then shuffle it into deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMING_END_PHASE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.cpcost)
+	e2:SetTarget(s.cptg)
+	e2:SetOperation(s.cpop)
+end
+	--Lists "Darklord" archetype
+s.listed_series={0xef}
+
+	--Pay 1000 LP
+function s.cpcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.
+	PayLPCost(tp,1000)
+end
+	--Check for "Darklord" spell/trap
+function s.cpfilter(c)
+	return c:IsSetCard(0xef) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToDeck() and c:CheckActivateEffect(false,true,false)~=nil
+end
+	--Activation legality
+function s.cptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		local te=e:GetLabelObject()
+		local tg=te:GetTarget()
+		return tg and tg(e,tp,eg,ep,ev,re,r,rp,0,chkc)
+	end
+	if chk==0 then return Duel.IsExistingTarget(s.cpfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.cpfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local te,ceg,cep,cev,cre,cr,crp=g:GetFirst():CheckActivateEffect(false,true,true)
+	Duel.ClearTargetCard()
+	g:GetFirst():CreateEffectRelation(e)
+	local tg=te:GetTarget()
+	if tg then tg(e,tp,ceg,cep,cev,cre,cr,crp,1) end
+	te:SetLabelObject(e:GetLabelObject())
+	e:SetLabelObject(te)
+	Duel.ClearOperationInfo(0)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+	--Copy the effect of 1 "Darklord" S/T, then shuffle it into deck
+function s.cpop(e,tp,eg,ep,ev,re,r,rp)
+	local te=e:GetLabelObject()
+	if not te then return end
+	if not te:GetHandler():IsRelateToEffect(e) then return end
+	e:SetLabelObject(te:GetLabelObject())
+	local op=te:GetOperation()
+	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	Duel.BreakEffect()
+	Duel.SendtoDeck(te:GetHandler(),nil,2,REASON_EFFECT)
+end
