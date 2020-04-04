3a6a214e243165c9a1e36d89ba34f2dc738781f1
LogicalNonsense2051 <43290281+LogicalNonsense2051@users.noreply.github.com>
2020-04-04 10:37:26 -0400

    Updated "Dark Liberation"
    Was not able to activate the card.

--- Rush Duel/script/c160301013.lua
+++ Rush Duel/script/c160301013.lua
@@ -1,20 +1,21 @@
--- Dark Liberation - ダーク・リベレイション
+--ダーク・リベレイション
+--Dark Liberation 
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+	--Shuffle 4 spellcasters into deck to destroy opponent's attack position monsters
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCondition(s.condition)
-	e1:SetCost(s.drcost)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 function s.cfilter(c)
-	return  c:IsRace(RACE_SPELLCASTER) and c:IsAbleToDeckAsCost()
+	return c:IsRace(RACE_SPELLCASTER) and c:IsAbleToDeckAsCost()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,4,nil) end
@@ -35,11 +36,11 @@ function s.chlimit(e,ep,tp)
 	return not e:IsHasType(EFFECT_TYPE_ACTIVATE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	-- requirement
+	--Requirement
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,4,4,nil)
 	Duel.SendtoDeck(g,nil,4,REASON_COST)
-	-- effect
+	--Effect
 	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
 	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
